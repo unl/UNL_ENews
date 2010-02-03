691a180f6923fef30a09a3bf7970350d3d735d30
Seth Meranda <smeranda2@unl.edu>
2010-02-03 17:26:42 +0000

    cleaned up markup

--- www/templates/ENews/Submission.tpl.php
+++ www/templates/ENews/Submission.tpl.php
@@ -16,30 +16,21 @@ function getValue($object, $field)
 //WDN.loadCSS('http://www.unl.edu/wdn/templates_3.0/css/content/forms.css');
 </script>
 <form class="cool" action="?view=submit" method="post">
-<fieldset class="wdn_collapsiblePanel" id="wdn_process_step1">
-<legend>Select E-News Type</legend>
-<ul>
-	<li><a href="#" id="newsAnnouncement">Is this a News announcment?</a></li>
-	<li><a href="#" id="eventAnnouncement">Is this an Event announcement?</a></li>
-</ul>
+<fieldset id="wdn_process_step1">
+	<legend>Select E-News Type</legend>
+	<ul>
+		<li><a href="#" id="newsAnnouncement">Is this a News announcment?</a></li>
+		<li><a href="#" id="eventAnnouncement">Is this an Event announcement?</a></li>
+	</ul>
 </fieldset>
-<fieldset class="wdn_collapsiblePanel" id="wdn_process_step2">
-<legend>Enter Date Details for Event</legend>
-
-    <input type="hidden" name="_type" value="story" />
-    <fieldset>
-        <legend>Date of Event</legend>
+<fieldset id="wdn_process_step2">
+	<legend>Enter Date Details for Event</legend>
         <ol>
         	<li><label for="title" class="element"><span class="required">*</span>Date of Event</label><div class="element"><input id="title" name="title" type="text" value="<?php echo getValue($context, 'title'); ?>" /></div></li>
         </ol>
-    </fieldset>
-
 </fieldset>
-<fieldset class="wdn_collapsiblePanel" id="wdn_process_step3">
-<legend>Give me the details</legend>
-    <input type="hidden" name="_type" value="story" />
-   
-        <legend>News Announcement Submission</legend>
+<fieldset id="wdn_process_step3">
+	<legend>News Announcement Submission</legend>
         <ol>
             <li><label for="title" class="element"><span class="required">*</span>Headline or Title</label><div class="element"><input id="title" name="title" type="text" value="<?php echo getValue($context, 'title'); ?>" /></div></li>
             <li><label for="description" class="element">Description</label><div class="element"><textarea id="description" name="description" cols="60" rows="5"><?php echo getValue($context, 'description'); ?></textarea></div></li>
@@ -63,13 +54,10 @@ function getValue($object, $field)
             </li>
             <li class="reqnote"><span class="required">*</span> denotes required field</li>
         </ol>
-    
-    
-
-<fieldset>
-	<legend>Event Announcement Submission</legend>
-    <p>Pull in the event form.</p>
-</fieldset>
+	<fieldset>
+		<legend>Event Announcement Submission</legend>
+	    <p>Pull in the event form.</p>
+	</fieldset>
 </fieldset>
 <p class="submit"><input type="submit" name="submit" value="Submit" /></p>
 </form>
