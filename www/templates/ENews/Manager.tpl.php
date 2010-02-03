556874e52cf072ed6bcd12968b43588ecad16dce
Brett Bieber <brett.bieber@gmail.com>
2010-02-03 03:44:07 +0000

    add the selected class to the appropriate tab

--- www/templates/ENews/Manager.tpl.php
+++ www/templates/ENews/Manager.tpl.php
@@ -1,9 +1,14 @@
 <ul class="wdn_tabs disableSwitching">
-    <?php foreach (array('pending', 'approved', 'archived') as $type): ?>
-    <li><a href="?view=manager&amp;type=<?php echo $type; ?>"><?php echo ucfirst($type);
-    if ($context->options['type'] == $type 
+    <?php foreach (array('pending', 'approved', 'archived') as $type):
+    $class = '';
+    if ($context->options['type'] == $type) {
+        $class = ' class="selected"';
+    }
+    ?>
+    <li <?php echo $class; ?>><a href="?view=manager&amp;type=<?php echo $type; ?>"><?php echo ucfirst($type);
+    if ($context->options['type'] == $type
         && count($context->actionable)): ?>
-    <sup><?php echo count($context->actionable); ?></sup>
+        <sup><?php echo count($context->actionable); ?></sup>
     <?php endif; ?></a></li>
     <?php endforeach; ?>
 </ul>
