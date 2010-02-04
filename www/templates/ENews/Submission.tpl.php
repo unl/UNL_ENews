2cacd1548cba7baaa595bea29a11361a3902a3b7
Seth Meranda <smeranda2@unl.edu>
2010-02-04 20:37:18 +0000

    UI for submit forms

--- www/templates/ENews/Submission.tpl.php
+++ www/templates/ENews/Submission.tpl.php
@@ -12,32 +12,40 @@ function getValue($object, $field)
     return '';
 }
 ?>
+<<<<<<< .mine
+<form action="?view=submit" method="post">
+<h3><span>1</span>Select E-News Type</h3>
+<fieldset id="wdn_process_step1" style="display:none;">
+=======
 <script type="text/javascript">
 //WDN.loadCSS('http://www.unl.edu/wdn/templates_3.0/css/content/forms.css');
 </script>
 <form class="cool" action="?view=submit" method="post" enctype="multipart/form-data">
 <fieldset id="wdn_process_step1">
+>>>>>>> .r36
 	<legend>Select E-News Type</legend>
-	<ul>
-		<li><a href="#" id="newsAnnouncement">Is this a News announcment?</a></li>
+	<ol class="option_step">
+		<li><a href="#" id="newsAnnouncement">Is this a News announcement?</a></li>
 		<li><a href="#" id="eventAnnouncement">Is this an Event announcement?</a></li>
-	</ul>
+	</ol>
 </fieldset>
-<fieldset id="wdn_process_step2">
-	<legend>Enter Date Details for Event</legend>
+<h3><span>2</span>Enter Date Details for Event</h3>
+<fieldset id="wdn_process_step2" style="display:none;">
+	<legend><span>Enter Date Details for Event</span></legend>
         <ol>
         	<li><label for="title" class="element"><span class="required">*</span>Date of Event</label><div class="element"><input id="title" name="title" type="text" value="<?php echo getValue($context, 'title'); ?>" /></div></li>
         </ol>
 </fieldset>
+<h3 class="highlighted"><span>3</span>Announcement Submission</h3>
 <fieldset id="wdn_process_step3">
+	<legend><span>News Announcement Submission</span></legend>
     <input type="hidden" name="_type" value="story" />
-	<legend>News Announcement Submission</legend>
         <ol>
-            <li><label for="title" class="element"><span class="required">*</span>Headline or Title</label><div class="element"><input id="title" name="title" type="text" value="<?php echo getValue($context, 'title'); ?>" /></div></li>
+            <li><label for="title" class="element">Headline or Title<span class="required">*</span></label><div class="element"><input id="title" name="title" type="text" value="<?php echo getValue($context, 'title'); ?>" /></div></li>
             <li><label for="description" class="element">Description</label><div class="element"><textarea id="description" name="description" cols="60" rows="5"><?php echo getValue($context, 'description'); ?></textarea></div></li>
-            <li><label for="event_date" class="element"><span class="required">*</span>Date and Time</label><div class="element"><input id="event_date" name="event_date" type="text" size="10"  value="<?php echo getValue($context, 'event_date'); ?>" /></div></li>
+            <li><label for="event_date" class="element">Date and Time<span class="required">*</span></label><div class="element"><input id="event_date" name="event_date" type="text" size="10"  value="<?php echo getValue($context, 'event_date'); ?>" /></div></li>
             <li><label for="website" class="element">Website</label><div class="element"><input id="website" name="website" type="text"  value="<?php echo getValue($context, 'website'); ?>" /></div></li>
-            <li><label for="sponsor" class="element"><span class="required">*</span>Sponsoring Unit</label><div class="element"><input id="sponsor" name="sponsor" type="text" value="<?php echo UNL_ENews_Controller::getUser()->unlHRPrimaryDepartment; ?>" /></div></li>
+            <li><label for="sponsor" class="element">Sponsoring Unit<span class="required">*</span></label><div class="element"><input id="sponsor" name="sponsor" type="text" value="<?php echo UNL_ENews_Controller::getUser()->unlHRPrimaryDepartment; ?>" /></div></li>
             <li><label for="image" class="element">Image</label><div class="element"><input id="image" name="image" type="file" /></div></li>
             <li>
                 <label class="element">Please consider for</label>
@@ -53,12 +61,21 @@ function getValue($object, $field)
                     <?php endforeach; ?>
                 </div>
             </li>
-            <li class="reqnote"><span class="required">*</span> denotes required field</li>
         </ol>
-	<fieldset>
-		<legend>Event Announcement Submission</legend>
-	    <p>Pull in the event form.</p>
-	</fieldset>
+</fieldset>
+<fieldset>
+	<legend>Event Announcement Submission</legend>
+    <p>Pull in the event form.</p>
 </fieldset>
 <p class="submit"><input type="submit" name="submit" value="Submit" /></p>
 </form>
+
+<script type="text/javascript">
+//WDN.loadCSS('http://www.unl.edu/wdn/templates_3.0/css/content/forms.css');
+WDN.jQuery(document).ready(function() { 
+	alert("yeah");
+	WDN.jQuery('legend span').width(
+			WDN.jQuery(this).parent('form').width();
+	);
+});
+</script>
