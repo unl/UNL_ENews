af37da2171a9e95281c6a6dfba67c168630226d5
Brett Bieber <brett.bieber@gmail.com>
2010-02-06 04:04:26 +0000

    Add a form to the newsletter preview. Prepare for posting newsletter details and saving.

--- www/templates/ENews/Newsletter/Preview.tpl.php
+++ www/templates/ENews/Newsletter/Preview.tpl.php
@@ -1,3 +1,8 @@
-<div>
-<?php echo $savvy->render($context->newsletter); ?>
-</div>
+<form method="post" action="?view=newsletter&amp;id=<?php echo $context->newsletter->id; ?>">
+    <input name="_type" value="newsletter" type="hidden" />
+    Email Subject: <input name="subject" size="100" value="<?php echo $context->newsletter->subject; ?>" />
+    <div>
+    <?php echo $savvy->render($context->newsletter); ?>
+    </div>
+    <input type="submit" value="Save" />
+</form>
