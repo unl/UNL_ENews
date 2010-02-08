60d65498e35c298919729150b08945021b64efe2
Seth Meranda <smeranda2@unl.edu>
2010-02-08 16:58:08 +0000

    change order of JS files, load UI first.

--- www/templates/ENews/Controller.tpl.php
+++ www/templates/ENews/Controller.tpl.php
@@ -18,8 +18,8 @@
 <link rel="stylesheet" type="text/css" media="screen" href="/wdn/templates_3.0/css/all.css" />
 <link rel="stylesheet" type="text/css" media="print" href="/wdn/templates_3.0/css/print.css" />
 <link rel="stylesheet" type="text/css" media="screen" href="css/all.css" />
-<script type="text/javascript" src="/wdn/templates_3.0/scripts/all.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="/wdn/templates_3.0/scripts/all.js"></script>
 <?php virtual('/wdn/templates_3.0/includes/browserspecifics.html'); ?>
 <?php virtual('/wdn/templates_3.0/includes/metanfavico.html'); ?>
 <!-- InstanceBeginEditable name="doctitle" -->
