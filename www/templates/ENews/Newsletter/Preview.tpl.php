3d54d24594d15dfbfed792328e08047fc1357369
Seth Meranda <smeranda2@unl.edu>
2010-02-09 16:34:08 +0000

    Updated UI for list of stories on the left when multiple are available, reformatted column layout to work for Mozilla, messing around with styles for the newsletter itself.

--- www/templates/ENews/Newsletter/Preview.tpl.php
+++ www/templates/ENews/Newsletter/Preview.tpl.php
@@ -5,21 +5,23 @@
     Email Subject: <input name="subject" size="100" value="<?php echo $context->newsletter->subject; ?>" />
     <input type="submit" value="Save" />
 </form>
-    <div class="col left zenbox energetic" id="drag_story_list">
-    <h3>Available News</h3>
-	<?php foreach ($context->available_stories as $story): ?>
-    	<div id="drag_story_<?php echo $story->id; ?>" class="dragItem">
-            <h4><?php echo $story->title; ?></h4>
-            <p><?php echo $story->description; ?></p>
-	        <form method="post" action="?view=newsletter&amp;newsletter_id=<?php echo $context->newsletter->id; ?>">
-            <input type="hidden" name="_type" value="addstory" />
-            <input type="hidden" name="story_id" value="<?php echo $story->id; ?>" />
-            <input type="hidden" name="sort_order" value="0" />
-            <input type="hidden" name="intro" value="" />
-            <input type="submit" value="add story" />
-        </form>
-        </div>
-	<?php endforeach; ?>
+    <div class="col left">
+    	<div class="zenbox energetic" id="drag_story_list"> 
+		    <h3>Available News</h3>
+			<?php foreach ($context->available_stories as $story): ?>
+		    	<div id="drag_story_<?php echo $story->id; ?>" class="dragItem">
+		            <h4><?php echo $story->title; ?></h4>
+		            <p><?php echo $story->description; ?></p>
+			        <form method="post" action="?view=newsletter&amp;newsletter_id=<?php echo $context->newsletter->id; ?>">
+		            <input type="hidden" name="_type" value="addstory" />
+		            <input type="hidden" name="story_id" value="<?php echo $story->id; ?>" />
+		            <input type="hidden" name="sort_order" value="0" />
+		            <input type="hidden" name="intro" value="" />
+		            <input type="submit" value="add story" />
+		        </form>
+		        </div>
+			<?php endforeach; ?>
+		</div>
     </div>
     <div class="three_col right">
     <?php echo $savvy->render($context->newsletter); ?>
