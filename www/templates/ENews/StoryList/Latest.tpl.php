2e4c776c7b3d3a2be8d10c9132fdeed0db81beeb
Brett Bieber <brett.bieber@gmail.com>
2010-02-12 14:26:27 +0000

    Clean up the latest news homepage. Use the same story styling as in the newsletter. Add link to submit news on homepage.

--- www/templates/ENews/StoryList/Latest.tpl.php
+++ www/templates/ENews/StoryList/Latest.tpl.php
@@ -1,6 +1,11 @@
-<h1>Latest E-News</h1>
-<ul>
-    <?php foreach($context as $item) :?>
-    <li><?php echo $savvy->render($item); ?></li>
-    <?php endforeach; ?>
-</ul>
+<div class="three_col left">
+    <h1>Latest E-News</h1>
+    <ul>
+        <?php foreach($context as $item) :?>
+        <li><?php echo $savvy->render($item, 'ENews/Newsletter/Story.tpl.php'); ?></li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<div class="col right" style="overflow:hidden;">
+    <a href="?view=submit">Submit News</a>
+</div>
