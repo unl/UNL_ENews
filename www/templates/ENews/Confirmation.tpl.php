5640af502be86b4d74485e830a317f07151e3224
Seth Meranda <smeranda2@unl.edu>
2010-02-12 14:45:54 +0000

    markup for the confimation notice enhancement

--- www/templates/ENews/Confirmation.tpl.php
+++ www/templates/ENews/Confirmation.tpl.php
@@ -1,7 +1,18 @@
-<?php if ($context->type == 'sendnews'): ?>
-    <h3>Distribution Successful.</h3>
-    <p>Your newsletter has been submitted to the distribution queue.</p>
-<?php else: ?>
-    <h3>Thank you for submitting your e-news/newstip.</h3>
-    <p>If you have any questions, please contact us.</p>
-<?php endif;?>
+<script type="text/javascript">
+// This plugin is only needed for the demo.
+WDN.initializePlugin('notice');
+</script>
+<div class="wdn_notice">
+	<div class="close">
+		<a href="#" title="Close this notice">Close this notice</a>
+	</div>
+	<div class="message">
+		<?php if ($context->type == 'sendnews'): ?>
+		    <h4>Distribution Successful.</h4>
+		    <p>Your newsletter has been submitted to the distribution queue.</p>
+		<?php else: ?>
+		    <h4>Thank you for submitting your e-news/newstip.</h4>
+		    <p>If you have any questions, please contact us.</p>
+		<?php endif;?>
+	</div>
+</div>
