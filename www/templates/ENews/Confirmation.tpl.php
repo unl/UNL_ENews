3b1849be75686d15518de5a075f216f81038c4b9
Brett Bieber <brett.bieber@gmail.com>
2010-02-12 14:17:15 +0000

    Add custom confirmation message for newsletter distribution.

--- www/templates/ENews/Confirmation.tpl.php
+++ www/templates/ENews/Confirmation.tpl.php
@@ -1,2 +1,7 @@
-<h3>Thank you for submitting your e-news/newstip.</h3>
-<p>If you have any questions, please contact us.</p>
+<?php if ($context->type == 'sendnews'): ?>
+    <h3>Distribution Successful.</h3>
+    <p>Your newsletter has been submitted to the distribution queue.</p>
+<?php else: ?>
+    <h3>Thank you for submitting your e-news/newstip.</h3>
+    <p>If you have any questions, please contact us.</p>
+<?php endif;?>
