23582b14869405e023f70f6ac1218df4ae3dd365
Brett Bieber <brett.bieber@gmail.com>
2010-02-12 14:35:55 +0000

    Add non-functioning delete button to the list of newsletters.

--- www/templates/ENews/Newsroom/Newsletters.tpl.php
+++ www/templates/ENews/Newsroom/Newsletters.tpl.php
@@ -6,6 +6,7 @@
             <th>Subject</th>
             <th>Edit</th>
             <th>Send</th>
+            <th>Delete</th>
         </tr>
     </thead>
     <tbody>
@@ -15,6 +16,13 @@
             <td><?php echo $newsletter->subject; ?></td>
             <td><a href="?view=newsletter&amp;id=<?php echo $newsletter->id; ?>">Edit</a></td>
             <td><a href="?view=sendnews&amp;id=<?php echo $newsletter->id; ?>">Send</a></td>
+            <td>
+                <form action="?view=newsletters" method="post" id="deletenewsletter" style="width:120px;">
+                    <input type="hidden" name="_type" value="deletenewsletter" />
+                    <input type="hidden" name="id" value="<?php echo $newsletter->id; ?>" />
+                    <a href="#" onclick="if (confirm('Are you sure?')) document.getElementById('deletenewsletter').submit();">Delete</a>
+                </form>
+            </td>
         </tr>
         <?php endforeach; ?>
     </tbody>
