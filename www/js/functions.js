87d7ba282045acb077e48c9b4f6bbec46dbec358
Seth Meranda <smeranda2@unl.edu>
2010-02-18 20:30:28 +0000

    allow the new intro section to accommodate news stories, update the order and send to DB. For some reason, it's not saving to the DB though?

--- www/js/functions.js
+++ www/js/functions.js
@@ -23,6 +23,8 @@ WDN.jQuery(document).ready(function() {
 		});
 		return false;
 	});
+	WDN.jQuery('#enews h3').eq(0).css('cursor','pointer').click(backToStep1);
+	WDN.jQuery('#enews h3').eq(1).css('cursor','pointer').click(backToStep2);
 	//Update the sample layout
 	WDN.jQuery('#title').keyup(function() {
 		var demoTitle = WDN.jQuery(this).val();
@@ -84,14 +86,17 @@ WDN.jQuery(document).ready(function() {
 			WDN.jQuery.post(WDN.jQuery(this).find('form').attr('action'), WDN.jQuery(this).find('form').serialize());
 		}
 	});
-	WDN.jQuery('#enews h3').eq(0).css('cursor','pointer').click(backToStep1);
-	WDN.jQuery('#enews h3').eq(1).css('cursor','pointer').click(backToStep2);
 });
 function saveStoryOrder() { //this function determines the order of the stories and sends it to the DB.
 	WDN.jQuery('#newsColumn1, #newsColumn2, #newsColumnIntro').sortable('refresh');
+	var resultIntro = WDN.jQuery('#newsColumnIntro').sortable('toArray');
 	var result1 = WDN.jQuery('#newsColumn1').sortable('toArray');
 	var result2 = WDN.jQuery('#newsColumn2').sortable('toArray');
-	for(var i = 0; i<result1.length; i++) {
+	for(var i = 0; i<resultIntro.length; i++) {
+		WDN.jQuery('#'+resultIntro[i]+' form input[name=sort_order]').attr('value', i*2);
+		WDN.jQuery.post(WDN.jQuery('#'+resultIntro[i]+' form').attr('action'), WDN.jQuery('#'+resultIntro[i]+' form').serialize());
+	}
+	for(i = 0; i<result1.length; i++) {
 		WDN.jQuery('#'+result1[i]+' form input[name=sort_order]').attr('value', i*2+1);
 		WDN.jQuery.post(WDN.jQuery('#'+result1[i]+' form').attr('action'), WDN.jQuery('#'+result1[i]+' form').serialize());
 	}
