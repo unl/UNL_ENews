ebd2ed8577811c0e6e571cfead905fe63fa6d0a3
Eric Rasmussen <erasmussen2@unl.edu>
2010-04-20 18:12:33 +0000

    add clearing divs on the newsletter web view to clear the floated thumbnails

--- www/templates/default/ENews/Newsletter.tpl.php
+++ www/templates/default/ENews/Newsletter.tpl.php
@@ -27,16 +27,19 @@
            <div id="newsletterWeb">            
                                 <div id="newsColumnIntro" class="newsColumn">
                                 <?php echo $columnIntro; ?>
+                                <div class="clear"></div>
                                 </div>
                      
                          <div style="width:340px;padding:0 10px 0 0;float:left;">
                                 <div id="newsColumn1" class="newsColumn">
                                 <?php echo $column1; ?>
+                                <div class="clear"></div>                                
                                 </div>
                          </div>
                          <div style="width:340px;padding:0 0 0 10px;float:right;">
                                 <div id="newsColumn2" class="newsColumn">
                                 <?php echo $column2; ?>
+                                <div class="clear"></div>
                                 </div>
                          </div>
   			</div>
--- www/templates/default/ENews/Newsletter/Story.tpl.php
+++ www/templates/default/ENews/Newsletter/Story.tpl.php
@@ -11,7 +11,7 @@ foreach ($context->getFiles() as $file) {
     if ($file->use == 'thumbnail') {
         echo '<img src="'.UNL_ENews_Controller::getURL().'?view=file&amp;id='
              . $file->id
-             . '" style="max-width:72px; margin-right:15px;" align="left" />';
+             . '" style="max-width:72px; margin-right:15px; float:left;" />';
     }
 }
 
