fe8422a3831f7637e7b70fe66bec3f2756b24c59
Brett Bieber <brett.bieber@gmail.com>
2010-04-22 00:53:40 +0000

    Whitespace and alignment

--- www/templates/default/ENews/Newsletter.tpl.php
+++ www/templates/default/ENews/Newsletter.tpl.php
@@ -3,47 +3,46 @@
     -
     <?php echo date('l F j, Y', strtotime($context->release_date)); ?>
 </h3>
-                        
-           
-                       
-                              
-                            <?php
-                            $columnIntro = '';
-                            $column1 = '';
-                            $column2 = '';
-                            foreach ($context->getStories() as $key=>$story) {
 
-                                $column = 'column1';
 
-                                if ($story->sort_order % 2 == 0) {
-                                    $column = 'column2';
-                                }
-                                if ($story->sort_order == 0) {
-                                	$column = 'columnIntro';
-                                }
+<?php
+$columnIntro = '';
+$column1     = '';
+$column2     = '';
 
-                                $$column .= '
-                                    <div class="story" id="story_'.$key.'">'
-                                	. $savvy->render($story) .'
-                                    </div>';
-                            }
-                            ?>
-           <div id="newsletterWeb">            
-                                <div id="newsColumnIntro" class="newsColumn">
-                                <?php echo $columnIntro; ?>
-                                <div class="clear"></div>
-                                </div>
-                     
-                         <div style="width:340px;padding:0 10px 0 0;float:left;">
-                                <div id="newsColumn1" class="newsColumn">
-                                <?php echo $column1; ?>
-                                <div class="clear"></div>                                
-                                </div>
-                         </div>
-                         <div style="width:340px;padding:0 0 0 10px;float:right;">
-                                <div id="newsColumn2" class="newsColumn">
-                                <?php echo $column2; ?>
-                                <div class="clear"></div>
-                                </div>
-                         </div>
-  			</div>
+foreach ($context->getStories() as $key=>$story) {
+
+    $column = 'column1';
+
+    if ($story->sort_order % 2 == 0) {
+        $column = 'column2';
+    }
+    if ($story->sort_order == 0) {
+        $column = 'columnIntro';
+    }
+
+    $$column .= '
+        <div class="story" id="story_'.$key.'">'
+        . $savvy->render($story) .'
+        </div>';
+}
+?>
+<div id="newsletterWeb">
+    <div id="newsColumnIntro" class="newsColumn">
+        <?php echo $columnIntro; ?>
+        <div class="clear"></div>
+    </div>
+    
+    <div style="width:340px;padding:0 10px 0 0;float:left;">
+        <div id="newsColumn1" class="newsColumn">
+            <?php echo $column1; ?>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <div style="width:340px;padding:0 0 0 10px;float:right;">
+        <div id="newsColumn2" class="newsColumn">
+            <?php echo $column2; ?>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
