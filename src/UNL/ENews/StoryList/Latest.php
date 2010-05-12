afcdcab2c946027913fd2fa706cdcd983f2d040c
Brett Bieber <brett.bieber@gmail.com>
2010-05-12 20:41:51 +0000

    Add some phpdoc comments

--- src/UNL/ENews/StoryList/Latest.php
+++ src/UNL/ENews/StoryList/Latest.php
@@ -1,6 +1,11 @@
 <?php
 class UNL_ENews_StoryList_Latest extends UNL_ENews_StoryList
 {
+    /**
+     * The newsroom which has the stories
+     * 
+     * @var UNL_ENews_Newsroom
+     */
     public $newsroom;
     
     function __construct($options = array())
@@ -11,7 +16,6 @@ class UNL_ENews_StoryList_Latest extends UNL_ENews_StoryList
         
         $stories = array();
         $mysqli = UNL_ENews_Controller::getDB();
-        //$sql = 'SELECT id FROM stories;';
         $sql = 'SELECT story_id FROM newsroom_stories WHERE status = "approved" AND newsroom_id = '.(int)$options['newsroom'];
         if ($result = $mysqli->query($sql)) {
             while($row = $result->fetch_array(MYSQLI_NUM)) {
