1fc9ccecf13fdc010cf4468b033ae63a885e6f1f
Eric Rasmussen <erasmussen2@unl.edu>
2010-05-18 18:26:31 +0000

    enhance individual story page

--- www/templates/default/ENews/Story.tpl.php
+++ www/templates/default/ENews/Story.tpl.php
@@ -1,17 +1,30 @@
 <div class="three_col left">
 <h3 class="sec_main"><?php echo $context->title; ?></h3>
-<p>
+
+<div style="float:right;width:310px;margin-bottom:20px;">
 <?php 
 foreach ($context->getFiles() as $file) {
     if (preg_match('/^image/', $file->type) && $file->use_for != 'thumbnail') {
         echo '<img src="'.UNL_ENews_Controller::getURL().'?view=file&amp;id='
              . $file->id
-             . '" style="max-width:300px; margin-left:15px; float:right;" class="frame" alt="'.$file->name.'" />';
+             . '" style="max-width:300px;float:left;" class="frame" alt="'.$file->name.'" />';
     }
 }
-
-echo $context->full_article;
 ?>
+</div>
+
+<?php $full = trim($context->full_article); ?>
+<?php if (!empty($full)) : ?>
+    <div style="background:#f0f0f0;width:350px;margin:0 10px 15px 0;padding:10px;font-style:italic;">
+    <?php echo $context->description;?>
+    </div>
+    <p>
+    <?php echo $context->full_article; ?>
+    </p>
+<?php else : ?>
+    <?php echo $context->description;?>
+<?php endif ?>
+
 </div>
 <div class="col right">
 
