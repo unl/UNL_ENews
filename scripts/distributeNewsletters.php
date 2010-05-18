d6bbf0a8e280de9162b30cf0cc441a3b20be8e40
Brett Bieber <brett.bieber@gmail.com>
2010-05-18 21:00:04 +0000

    Store which newsletters have been distributed.

--- scripts/distributeNewsletters.php
+++ scripts/distributeNewsletters.php
@@ -10,4 +10,6 @@ $enews = new UNL_ENews_Controller();
 
 foreach (UNL_ENews_NewsletterList::getUndistributed() as $newsletter) {
     $newsletter->distribute();
+    $newsletter->distributed = 1;
+    $newsletter->save();
 }
