6d6ca6a411af4ec5875ca4dc2dfdd38b3042c780
Brett Bieber <brett.bieber@gmail.com>
2010-05-20 21:49:03 +0000

    correct path to config file

--- scripts/distributeNewsletters.php
+++ scripts/distributeNewsletters.php
@@ -1,9 +1,9 @@
 <?php
 
-if (file_exists(dirname(__FILE__).'/../config.inc.php')) {
-    require_once dirname(__FILE__).'/../config.inc.php';
+if (file_exists(dirname(__FILE__).'/../www/config.inc.php')) {
+    require_once dirname(__FILE__).'/../www/config.inc.php';
 } else {
-    require dirname(__FILE__).'/../config.sample.php';
+    require dirname(__FILE__).'/../www/config.sample.php';
 }
 
 $enews = new UNL_ENews_Controller();
