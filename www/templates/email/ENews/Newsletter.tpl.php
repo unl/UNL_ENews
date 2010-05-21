7848752a60b2f6e626f1b17e1726ba1ee585abca
Brett Bieber <brett.bieber@gmail.com>
2010-05-21 03:09:41 +0000

    Revert font-size:0. Some clients don't render text.

--- www/templates/email/ENews/Newsletter.tpl.php
+++ www/templates/email/ENews/Newsletter.tpl.php
@@ -9,16 +9,16 @@
         <!-- [ header starts here] -->
         <table cellspacing="0" cellpadding="0" border="0" width="600">
             <tr>
-                <td valign="top" style="font-size: 0px;"><img src="http://www.unl.edu/wdn/templates_3.0/images/email/header.png" alt="The University of Nebraska-Lincoln" width="600" height="126" border="0"/></td>
+                <td valign="top"><img src="http://www.unl.edu/wdn/templates_3.0/images/email/header.png" alt="The University of Nebraska-Lincoln" width="600" height="126" border="0"/></td>
             </tr>
         </table>
         <!-- [ middle starts here] -->
-        <table cellspacing="0" cellpadding="0" border="0" width="600" style="margin:0;font-size:0">
+        <table cellspacing="0" cellpadding="0" border="0" width="600">
             <tr>
-                <td width="12" bgcolor="#E0E0E0" style="margin:0;font-size:0">&nbsp;</td>
-                <td width="10" style="margin:0;font-size:0">&nbsp;</td>
-                <td align="left" valign="top" width="556" style="margin:0;font-size:0">
-                    <table cellspacing="0" cellpadding="0" border="0" style="margin:0;font-size:0">
+                <td width="12" bgcolor="#E0E0E0">&nbsp;</td>
+                <td width="10">&nbsp;</td>
+                <td align="left" valign="top" width="556">
+                    <table cellspacing="0" cellpadding="0" border="0">
                         <tr>
                             <td colspan="3" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
                                 <!-- This is the main content -->
@@ -84,16 +84,16 @@
                         </tr>
                     </table>
                 </td>
-                <td width="10" style="margin:0;font-size:0"></td>
-                <td width="12" bgcolor="#E0E0E0" style="margin:0;font-size:0"></td>
+                <td width="10"></td>
+                <td width="12" bgcolor="#E0E0E0"></td>
             </tr>
         </table>
-        <table cellspacing="0" cellpadding="0" border="0" width="600" style="margin:0;font-size:0">
+        <table cellspacing="0" cellpadding="0" border="0" width="600">
             <tr>
-                <td style="font-size: 0px;margin:0;"><img src="http://www.unl.edu/wdn/templates_3.0/images/email/footer.png" alt="The University of Nebraska-Lincoln" width="600" height="22" /></td>
+                <td><img src="http://www.unl.edu/wdn/templates_3.0/images/email/footer.png" alt="The University of Nebraska-Lincoln" width="600" height="22" /></td>
             </tr>
         </table>
     </td>
-</tr>
-</table>
+    </tr>
+    </table>
 </div>
