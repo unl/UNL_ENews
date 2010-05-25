ff89fadd29c7f5c88c2593ad1cd7ade8519dbce5
Brett Bieber <brett.bieber@gmail.com>
2010-05-25 14:15:51 +0000

    If the newsletter is unpublished, check permissions first.

--- src/UNL/ENews/Newsletter/Public.php
+++ src/UNL/ENews/Newsletter/Public.php
@@ -12,6 +12,16 @@ class UNL_ENews_Newsletter_Public
     {
         if (isset($options['id'])) {
             $this->newsletter = UNL_ENews_Newsletter::getById($options['id']);
+            if (!$this->newsletter) {
+                throw new Exception('Could not find that newsletter', 404);
+            }
+
+            // If this is an unpublished newsletter, check permissions
+            if ((empty($this->newsletter->release_date)
+                || ($this->newsletter->release_date > date('Y-m-d H:i:s')))
+                && !UNL_ENews_Controller::getUser(true)->hasPermission($this->newsletter->newsroom_id)) {
+                throw new Exception('You do not have permission to view unpublished newsletters for this newsroom', 403);
+            }
         } else {
             $this->newsletter = UNL_ENews_Newsletter::getLastReleased();
         }
