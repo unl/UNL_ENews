456f8352d13b946723fc5f5757257b4706a7e91d
Seth Meranda <smeranda2@unl.edu>
2010-06-24 16:28:46 +0000

    show the URL that was removed in the last committ.

--- www/templates/email/ENews/Newsletter/Story.tpl.php
+++ www/templates/email/ENews/Newsletter/Story.tpl.php
@@ -22,7 +22,7 @@ if (!empty($context->full_article)) {
 <table cellspacing="0" cellpadding="3" border="0" valign="top" bgcolor="#f7f6f6" width="100%">
 <tr>
 <td>
-More details at: <a href="<?php echo $context->website; ?>" title="Go to the supporting webpage"></a>
+More details at: <a href="<?php echo $context->website; ?>" title="Go to the supporting webpage"><?php echo $context->website; ?></a>
 </td>
 </tr>
 </table>
