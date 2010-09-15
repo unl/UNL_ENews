8325b3ebad135e5dfb0814260fdd0f85070e76b4
Brett Bieber <brett.bieber@gmail.com>
2010-09-15 01:07:32 +0000

    correct if statement. We are checking the resulting value once  has been set.

--- www/templates/default/ENews/Submission.tpl.php
+++ www/templates/default/ENews/Submission.tpl.php
@@ -198,7 +198,7 @@ function getValue($object, $field)
             </li></ol>
             
             <div id="upload_area">
-            <?php if ($id ==getValue($context,"id")) { ?>
+            <?php if ($id = getValue($context, "id")) { ?>
                     <?php if ($image = UNL_ENews_Story::getByID($id)->getFileByUse('originalimage')) { ?>
                             <img src="<?php echo UNL_ENews_Controller::getURL().'?view=file&id='.$image->id; ?>" alt="Image to accompany story submission" />
                             <span><script type="text/javascript">document.write(ajaxUpload.message);</script></span>
