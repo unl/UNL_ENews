ec5a72f2744146b4bd64e180ab131f75ef367363
Michael Fairchild <mfairchild365@gmail.com>
2010-09-24 13:36:44 +0000

    cleaned up ENews/Newsletter/Story.php

--- src/UNL/ENews/Newsletter/Story.php
+++ src/UNL/ENews/Newsletter/Story.php
@@ -32,26 +32,21 @@ class UNL_ENews_Newsletter_Story extends UNL_ENews_Record
     function __construct($options = array())
     {
         //Check to make sure the story is valid for the given newsroom.
-        if(isset($options['newsletter_id'])){
-            if($this->newsletter = UNL_ENews_Newsletter::getById($options['newsletter_id'])){
-                if($this->story = UNL_ENews_Story::getById($options['id'])){
-                    if($this->newsletter->hasStory($this->story)){
-                        if($this->newsroom = UNL_ENews_Newsroom::getByID($this->newsletter->newsroom_id)){
-                            if($this->newsroom->shortname == $options['shortname']){
-                            }else{
-                                throw new Exception('Not a valid news room name.');
-                            }
-                        }else{
-                            throw new Exception('The newsroom does not exist!');
-                        }
-                    }else{
-                        throw new Exception('The story does not belong to the given newsletter.');
-                    }
-                }else{
-                    throw new Exception('This story does not exist.');
-                }
-            }else{
-                throw new Exception('This newsletter does not exist');
+        if (isset($options['newsletter_id'])) {
+            if (!($this->newsletter = UNL_ENews_Newsletter::getById($options['newsletter_id']))) {
+                throw new Exception('This newsletter does not exist', 404);
+            }
+            if (!($this->story = UNL_ENews_Story::getById($options['id']))) {
+                throw new Exception('This story does not exist.', 400);
+            }
+            if (!($this->newsletter->hasStory($this->story))) {
+                throw new Exception('The story does not belong to the given newsletter.', 400);
+            }
+            if (!($this->newsroom = UNL_ENews_Newsroom::getByID($this->newsletter->newsroom_id))) {
+                throw new Exception('The newsroom does not exist!', 404);
+            }
+            if (!($this->newsroom->shortname == $options['shortname'])) {
+                throw new Exception('Not a valid news room name.', 400);
             }
         }
     }
