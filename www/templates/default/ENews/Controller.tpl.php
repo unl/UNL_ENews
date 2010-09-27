4ec990ac9ecd072fef9adbd1f98c47bbff2b1b72
Brett Bieber <brett.bieber@gmail.com>
2010-09-27 20:22:38 +0000

    Use absolute URL to "build newsletter"

--- www/templates/default/ENews/Controller.tpl.php
+++ www/templates/default/ENews/Controller.tpl.php
@@ -94,7 +94,7 @@ if ($context->options['view'] == 'story'
                         }
                         ?>
                     </li>
-                    <li><a href="?view=preview">Build Newsletter</a>
+                    <li><a href="<?php echo UNL_ENews_Controller::getURL(); ?>?view=preview">Build Newsletter</a>
                         <?php
                         if ($newsletters = $user->newsroom->getNewsletters(array('limit'=>3))) {
                             if (count($newsletters)) {
