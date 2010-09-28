0c12916a5001ffd46ae54d62615be4dec1a9f0e5
Brett Bieber <brett.bieber@gmail.com>
2010-09-28 15:08:39 +0000

    Because we use the same template for stories that aren't in a newsletter, only pull the URL if the newsletter is available.

--- www/templates/email/ENews/Newsletter/Story.tpl.php
+++ www/templates/email/ENews/Newsletter/Story.tpl.php
@@ -1,5 +1,8 @@
 <?php
-$url = $context->newsletter->getURL().'/'.$context->id;
+$url = '';
+if (isset($context->newsletter)) {
+    $url = $context->newsletter->getURL().'/'.$context->id;
+}
 ?>
 <h4><a style="color:#666; text-decoration:none;" href="<?php echo $url ?>"><?php echo $context->title; ?></a></h4>
 
