3c1abc40c502f32d51287c237a86b360c9960eac
Eric Rasmussen <erasmussen2@unl.edu>
2010-11-24 21:38:50 +0000

    Default to newsroom 1 if not set in URL by shortname or newsroom=

--- src/UNL/ENews/Submission.php
+++ src/UNL/ENews/Submission.php
@@ -2,9 +2,9 @@
 class UNL_ENews_Submission extends UNL_ENews_LoginRequired
 {
     protected $story;
-    
+
     public $newsroom;
-    
+
     /**
      * Get the list of newsrooms that allow submissions.
      * 
@@ -14,21 +14,24 @@ class UNL_ENews_Submission extends UNL_ENews_LoginRequired
     {
         return new UNL_ENews_NewsroomList_AllowSubmissions();
     }
-    
+
     function __postConstruct()
-    {   
-        if(isset($this->options['shortname'])){
+    {
+        if (isset($this->options['shortname'])) {
             if (!$this->newsroom = UNL_ENews_Newsroom::getByShortname($this->options['shortname'])) {
-                throw new Exception("Newsroom not found", 404);
+                throw new Exception('Newsroom not found', 404);
             }
-        }else{
+        } else if (isset($this->options['newsroom'])) {
             if (!$this->newsroom = UNL_ENews_Newsroom::getByID($this->options['newsroom'])) {
-                throw new Exception("Newsroom not found", 404);
+                throw new Exception('Newsroom not found', 404);
+            }
+        } else {
+            if (!$this->newsroom = UNL_ENews_Newsroom::getByID(1)) {
+                throw new Exception('Ruh-roh, there is no newsroom 1', 404);
             }
         }
-        
-        if (isset($this->options['id'])) {
 
+        if (isset($this->options['id'])) {
             if (!$this->story = UNL_ENews_Story::getByID($this->options['id'])) {
                 throw new Exception('Could not find the story you were trying to edit!', 404);
             }
@@ -41,12 +44,12 @@ class UNL_ENews_Submission extends UNL_ENews_LoginRequired
             $this->story = new UNL_ENews_Story();
         }
     }
-    
+
     function __get($var)
     {
         return $this->story->$var;
     }
-    
+
     function __isset($var)
     {
         return isset($this->story->$var);
