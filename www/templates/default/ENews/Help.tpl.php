c198457c814453b5ce97b1cb194754078d5dda0c
Seth Meranda <smeranda2@unl.edu>
2010-11-30 16:56:39 +0000

    style the definition list, fix a few grammatical errors.

--- www/templates/default/ENews/Help.tpl.php
+++ www/templates/default/ENews/Help.tpl.php
@@ -1,52 +1,57 @@
 <style type="text/css">
 dt {
-     font-weight: bold; 
-     margin: 15px 50px 5px;
+    font-weight: bold;
+    font-size:1.3em;
+    padding:10px 0 0 0;
+    margin-left:60px;
+    border-bottom:1px solid #efefef;
 }
-
 dt:before {
-     content: "Q";
+     content: "Q:";
      font-size: 2.5em;
      font-family: Georgia, "Times New Roman", Times, serif;
      margin-right: 7px;
      padding: 0 8px 5px 8px;
-     color: #FFF;
-     background-color: #C20403;
+     color: #ccc;
      font-weight: normal;
-     margin-left: -47px;
+     margin-left: -65px;
      position: relative;
-     top: 13px;
+     top: 3px;
 }
  
 dd {
-     margin: 25px 50px 0px;
-     border-bottom: 1px solid #ccc;
-     padding-bottom: 20px;
-     line-height: 150%;
+    padding:10px 0 0 0;
+    margin-left:94px;
+    line-height: 150%;
+    min-height:50px;
 }
 
 dd:before {
-     content: "A";
+     content: "A:";
      font-size: 2.5em;
      font-family: Georgia, "Times New Roman", Times, serif;
-     margin-right: 7px;
+     margin-right: 14px;
      padding: 0 9px;
      color: #FFF;
-     background-color: #028A02;
-     margin-left: -47px;
+     background-color: #ccc;
+     margin-left: -65px;
      position: relative;
      top: 13px;
+     border-radius:3px;
+     -moz-border-radius:3px;
+     -webkit-border-radius:3px;
 }
+#maincontent dd p+ul {padding-left:20px;}
 </style>
 
 <dl>
 <dt>How do I submit a news/event announcement for consideration in Today@UNL?</dt>
 <dd>
-    <p>From <a href="<?php echo UNL_ENews_Controller::getURL(); ?>">UNL Announce</a>, select <a href="<?php echo UNL_ENews_Controller::getURL(); ?>?view=submit">Submit A News Item</a></p>
+    From <a href="<?php echo UNL_ENews_Controller::getURL(); ?>">UNL Announce</a>, select <a href="<?php echo UNL_ENews_Controller::getURL(); ?>?view=submit">Submit A News Item</a>:
     <ul>
         <li>Is your item a News Announcement or an Event Announcement?</li>
-        <li>Examples of news: grant, honor, award, feature story, discovery, new project</li>
-        <li>Examples of events:  Anything with a date/time/place attached to it: Lecture, seminar, conference, performance, concert, show, program, open house, reception</li>
+        <li>Examples of news: grant, honor, award, feature story, discovery, new project.</li>
+        <li>Examples of events:  Anything with a date/time/place attached to it: Lecture, seminar, conference, performance, concert, show, program, open house, reception.</li>
     </ul>
 </dd>
 
@@ -54,25 +59,25 @@ dd:before {
 <dd>
     Sure. Like announcing a series of lectures or tickets/reservations open for some future event?
     <ul>
-        <li>Enter it as a News Announcement, then add the specific dates and topics into the Events Announcement</li>
-        <li>Look at the submission forms when you click on each; they are slightly different. The News Announcement gives you more detail areas</li>
+        <li>Enter it as a News Announcement, then add the specific dates and topics into the Events Announcement.</li>
+        <li>Look at the submission forms when you click on each; they are slightly different. The News Announcement gives you more detail areas.</li>
     </ul>
 </dd>
 
 <dt>Under the Event Announcement submission, it asks me "Which Event" when I entered the date. Why?</dt>
 <dd>
-Either enter the event as a new event, or, before you get to this screen, enter your event in the Events calendar &ndash; this interface pulls events from the <a href="http://events.unl.edu/">Events calendar</a>
+Either enter the event as a new event, or, before you get to this screen, enter your event in the Events calendar &ndash; this interface pulls events from the <a href="http://events.unl.edu/">Events calendar</a>.
 </dd>
 
 <dt>What's a summary and how long is a summary?</dt> 
 <dd>
-Ideally a summary is the few sentences that will appear in the e-newsletter that serves as a teaser/mini headline to get readers interested or give them the gist and no more. As you type, you will see the summary appear in the box on the right. The best summaries are very short &ndash; one or two sentences &ndash; and give the basic facts. This interface limits summaries to 300 characters.
+Ideally a summary is the few sentences that will appear in the e-newsletter that serves as a teaser/mini headline to get readers interested or give them the gist and no more. As you type, you will see the summary appear in the box on the right. The best summaries are very short &ndash; one or two sentences &ndash; and give the basic facts. Summaries are limited to 300 characters.
 </dd>
 
 
 <dt>Full article: What does that mean?</dt>
 <dd>
-    <p>Say you have an entire news release or feature story that you want to submit.</p> 
+    Say you have an entire news release or feature story that you want to submit.
     <ul>
         <li>Copy and paste it into "Full Article" </li>
         <li>This gives editors more that they can use in other ways and in other channels: UNL Today, Scarlet, or as a news release or pitch. If you've written it, submit it. Use the summary box for the edited/shortened version</li>
@@ -83,9 +88,9 @@ Ideally a summary is the few sentences that will appear in the e-newsletter that
 <dd>
     <ul>
         <li>Your suggestions are helpful, and particularly useful if there is a deadline for entering, or you desire your event announcement to precede your event by a few days to give people time to plan to attend.</li> 
-        <li>Keep in mind that space and time constraints will dictate some of these decisions; we'll do our best to honor requests</li>
-        <li>Also remember that if you've entered your event in the UNL calendar, the events of that day will be part of the daily calendar</li>
-        <li>The "last date this could run" typically is the last day before your event is over</li>
+        <li>Keep in mind that space and time constraints will dictate some of these decisions; we'll do our best to honor requests.</li>
+        <li>Also remember that if you've entered your event in the UNL calendar, the events of that day will be part of the daily calendar.</li>
+        <li>The "last date this could run" typically is the last day before your event is over.</li>
     </ul>
 </dd>
 
@@ -97,7 +102,7 @@ Ideally a summary is the few sentences that will appear in the e-newsletter that
 <dd>
     <ul>
         <li>The system reads who you are when you enter, and defaults to your "unit" after you've entered information.</li>
-        <li>If you're entering something for another unit, change it so it's accurate</li>
+        <li>If you're entering something for another unit, change it so it's accurate.</li>
     </ul>
 </dd>
 
@@ -105,26 +110,26 @@ Ideally a summary is the few sentences that will appear in the e-newsletter that
 <dd>
     <ul>
         <li>Photos with action or plenty of visual appeal are best.</li>
-        <li>If you have a speaker or event highlighting one or two people, submit "head and shoulders" shots</li>
+        <li>If you have a speaker or event highlighting one or two people, submit "head and shoulders" shots.</li>
         <li>Provide good quality color photos, saved as RGB jpeg/jpg images and have them already tightly cropped, in good focus with good contrast.</li> 
-        <li>Type treatments, word marks and logos (university-approved) will be considered</li>
+        <li>Type treatments, word marks and logos (university-approved) will be considered.</li>
     </ul>
 </dd>
 
 <dt>What's the deadline for submissions?</dt>
 <dd>
     <ul>
-        <li>During the summer we'll e-mail twice a week (Tuesdays and Fridays) &ndash; submit items no later than noon the prior day </li>
-        <li>During the fall, delivery will be every day Monday-Friday (not university holidays) &ndash; submit items no later than noon the prior day</li>
+        <li>During the summer we'll e-mail twice a week (Tuesdays and Fridays) &ndash; submit items no later than noon the prior day.</li>
+        <li>During the fall, delivery will be every day Monday-Friday (not university holidays) &ndash; submit items no later than noon the prior day.</li>
     </ul>
 </dd>
 
 <dt>What if I have some late-breaking news that I just found out about that I really need to get out and it's after deadline?</dt>
-<dd>This is probably a news judgment call that we can help with: Call Troy Fedderson, 472-8515 or Kelly Bartling, 472-2059. Most likely, it can wait until the next day; please remember to plan ahead</dd>
+<dd>This is probably a news judgment call that we can help with: Call Troy Fedderson, 472-8515 or Kelly Bartling, 472-2059. Most likely, it can wait until the next day; please remember to plan ahead.</dd>
 
 <dt>Can I use this site to submit news releases or Scarlet features and briefs (instead of e-mailing them to Tom Simons or Troy?</dt>
 <dd>
-Yes. Until we all get used to this change, it's best to call ahead and explain your request
+Yes. Until we all get used to this change, it's best to call ahead and explain your request.
 </dd>
 
 <!--<dt>-->
