32b0e3237f5c47593cfb637044b53fdf32ddf485
Brett Bieber <brett.bieber@gmail.com>
2010-12-06 04:13:09 +0000

    Take advantage of the getURL() method so we have clean urls to the stories.

--- www/templates/text/ENews/Newsletter/Story.tpl.php
+++ www/templates/text/ENews/Newsletter/Story.tpl.php
@@ -1,13 +1,11 @@
 <?php
-    $newsletter_id = $parent->context->id;
-
 	echo $context->title . "\n";
 
 	echo $context->description . "\n\n";
 
 	echo $context->website . "\n";
 	
-	echo "Full Story: " . UNL_ENews_Controller::$url . "?view=story&id=" . $context->id . "\n";
+	echo "Full Story: " . $context->getURL() . "\n";
 	
 	echo "-------------------------------------------------\n\n";
 ?>
