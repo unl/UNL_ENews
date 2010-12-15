652c1d676e2cac0c1ef55becadc41083c7a1f21b
Brett Bieber <brett.bieber@gmail.com>
2010-12-15 22:15:00 +0000

    Set up the iterator for the emails associated with a newsletter.

--- src/UNL/ENews/Newsletter/Emails.php
+++ src/UNL/ENews/Newsletter/Emails.php
@@ -1,8 +1,29 @@
 <?php
-class UNL_ENews_Newsletter_Emails
+class UNL_ENews_Newsletter_Emails extends ArrayIterator implements Countable
 {
     function __construct($options = array())
     {
-        
+        if (!isset($options['newsletter_id'])) {
+            throw new Exception('Whoah, what newsletter do you expect me to use here?');
+        }
+        $emails = array();
+        $mysqli = UNL_ENews_Controller::getDB();
+        $sql = 'SELECT id FROM newsletter_emails ';
+        $sql .= ' WHERE newsletter_id = '.(int)$options['newsletter_id'];
+        if ($result = $mysqli->query($sql)) {
+            while($row = $result->fetch_array(MYSQLI_NUM)) {
+                $emails[] = $row[0];
+            }
+        }
+        $mysqli->close();
+        parent::__construct($emails);
+    }
+    
+    /**
+     * @return UNL_ENews_Story
+     */
+    function current()
+    {
+        return UNL_ENews_Newsroom_Email::getByID(parent::current());
     }
 }
