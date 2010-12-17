ee070a0e16128d18cdee0d34dc1d825bbac11503
Michael Fairchild <mfairchild365@gmail.com>
2010-12-17 17:13:16 +0000

    Added text for sucessfully unsubsribing from a newsletter.

--- www/templates/default/ENews/Confirmation.tpl.php
+++ www/templates/default/ENews/Confirmation.tpl.php
@@ -10,6 +10,9 @@ WDN.initializePlugin('notice');
 		<?php if ($context->options['_type'] == 'sendnews'): ?>
 		    <h4>Oh yeah! Distribution Successful!</h4>
 		    <p>Your newsletter has been submitted to the distribution queue.</p>
+		<?php elseif ($context->type == 'sendnews'): ?>
+		    <h4>Congratulations!</h4>
+		    <p>You have been unsubscribed from the newsletter!</p>
 		<?php else: ?>
 		    <h4>Thanks for your submission!</h4>
 		    <p>Your article is now in our queue. We will review, adapt and incorporate to the best of our abilities. If we have any questions, we'll contact you. If you have any questions, please contact us.</p>
