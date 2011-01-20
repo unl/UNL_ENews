22fcbb8f40d8a315058fb7a99cf493f3179516bb
Brett Bieber <brett.bieber@gmail.com>
2011-01-20 17:04:36 +0000

    Add alt text to image data.

--- www/templates/default/ENews/Newsroom.tpl.php
+++ www/templates/default/ENews/Newsroom.tpl.php
@@ -58,7 +58,7 @@
     <ul id="userList">
     <?php
     foreach ($context->getUsers() as $user) {
-        echo '<li><img class="profile_pic medium" src="http://planetred.unl.edu/pg/icon/unl_'.$user->uid.'/medium/" />
+        echo '<li><img class="profile_pic medium" src="http://planetred.unl.edu/pg/icon/unl_'.$user->uid.'/medium/" alt="Planet Red photo for '.$user->uid.'" />
         <form action="?view=newsroom" method="post">
             <input type="hidden" name="newsroom_id" value="'.$context->id.'" />
             <input type="hidden" name="_type" value="removeuser" />
