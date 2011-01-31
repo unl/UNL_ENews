938a83fda359294cd5b24eb3fc20aefad24dd2e1
Kevin Abel <kabel2@unl.edu>
2011-01-31 19:51:20 +0000

    Added new methods to the Newsletter_Stories model to allow for better template abstraction at render time

--- src/UNL/ENews/Newsletter/Stories.php
+++ src/UNL/ENews/Newsletter/Stories.php
@@ -1,13 +1,15 @@
 <?php
 class UNL_ENews_Newsletter_Stories extends UNL_ENews_StoryList
 {
-    
+
     protected $newsletter_id;
+
+    protected $isPreview = false;
 
     function __construct($options = array())
     {
         $this->newsletter_id = (int)$options['newsletter_id'];
-        
+
         $stories = array();
         $mysqli = UNL_ENews_Controller::getDB();
         $sql = 'SELECT story_id FROM newsletter_stories ';
@@ -21,10 +23,47 @@ class UNL_ENews_Newsletter_Stories extends UNL_ENews_StoryList
         $mysqli->close();
         parent::__construct($stories);
     }
-    
-    
+
+
     function current()
     {
         return UNL_ENews_Newsletter_Story::getById($this->newsletter_id, parent::current()->id);
+    }
+
+    function getStoriesByColumn()
+    {
+        $areas = array(
+            'news' => array(
+                0 => array(),
+                1 => array(),
+                2 => array()
+            ),
+            'ads'  => array(
+                0 => array(),
+                1 => array(),
+                2 => array()
+            )
+        );
+
+        foreach ($this as $story) {
+            $areaPtr =& $areas['news'];
+            if ($story->getPresentation()->type == 'ad') {
+                $areaPtr =& $areas['ads'];
+            }
+
+            $areaPtr[$story->getSortOrderOffset()][] = $story;
+        }
+
+        return $areas;
+    }
+
+    function setIsPreview($isPreview)
+    {
+        $this->isPreview = $isPreview;
+    }
+
+    function getIsPreview()
+    {
+        return $this->isPreview;
     }
 }
