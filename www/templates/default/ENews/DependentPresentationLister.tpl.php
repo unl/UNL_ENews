c3436bb0720be21dbe0415d5f8717f68d5585fdd
Kevin Abel <kabel2@unl.edu>
2011-01-31 17:14:55 +0000

    * Removed the added view/temaplte for undocumented & unneeded Dependent Presentations

--- www/templates/default/ENews/DependentPresentationLister.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-//header('Content-type: application/json');
-$json = array();
-foreach ($context->presentation_list as $presentation) {
-    $json[$presentation->id] = $presentation->dependent_selector;
-}
-echo json_encode($json, JSON_FORCE_OBJECT);
