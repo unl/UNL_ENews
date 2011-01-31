7ba600e133b174202289482e6c98bfc36b11e9c5
Kevin Abel <kabel2@unl.edu>
2011-01-31 22:15:36 +0000

    Updated the StoriesWeb template to use the StoryColumn template.

--- www/templates/default/ENews/Newsletter/StoriesWeb.tpl.php
+++ www/templates/default/ENews/Newsletter/StoriesWeb.tpl.php
@@ -1,80 +1,55 @@
-<?php
-$columns = array();
-$adAreas = array();
-foreach ($context as $key=>$story) {
-    $areaPtr =& $columns;
-    if ($story->getPresentation()->type == 'ad') {
-        $areaPtr =& $adAreas;
-    }
+<?php $areas = $context->getStoriesByColumn(); ?>
 
-    $areaPtr[$story->sort_order % 3][] = $story;
-}
-?>
-<div id="newsColumnIntro" class="newsColumn">
-<?php if (isset($columns[1])): ?>
-<?php foreach ($columns[1] as $story): ?>
-    <div class="story" id="story_<?php echo $story->story_id; ?>">
-        <?php echo $savvy->render($story, 'ENews/Newsletter/Story/Presentation/'.$story->getPresentation()->template); ?>
-        <div class="clear"></div>
-    </div>
-<?php endforeach; ?>
-<?php endif; ?>
-</div>
+<?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
+    'id' => 'newsColumnIntro',
+    'class' => 'newsColumn',
+    'stories' => $areas['news'][1],
+    'web' => true
+))); ?>
 
 <div style="width:340px;padding:0 10px 0 0;float:left;">
-    <div id="newsColumn1" class="newsColumn">
-    <?php if (isset($columns[2])): ?>
-    <?php foreach ($columns[2] as $story): ?>
-        <div class="story" id="story_<?php echo $story->story_id; ?>">
-            <?php echo $savvy->render($story, 'ENews/Newsletter/Story/Presentation/'.$story->getPresentation()->template); ?>
-            <div class="clear"></div>
-        </div>
-    <?php endforeach; ?>
-    <?php endif; ?>
-    </div>
+    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
+        'id' => 'newsColumn1',
+        'class' => 'newsColumn',
+        'stories' => $areas['news'][2],
+    	'web' => true
+    ))); ?>
 </div>
 <div style="width:340px;padding:0 0 0 10px;float:right;">
-    <div id="newsColumn2" class="newsColumn">
-    <?php if (isset($columns[0])): ?>
-    <?php foreach ($columns[0] as $story): ?>
-        <div class="story" id="story_<?php echo $story->story_id; ?>">
-            <?php echo $savvy->render($story, 'ENews/Newsletter/Story/Presentation/'.$story->getPresentation()->template); ?>
-            <div class="clear"></div>
-        </div>
-    <?php endforeach; ?>
-    <?php endif; ?>
-    </div>
+    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
+        'id' => 'newsColumn2',
+        'class' => 'newsColumn',
+        'stories' => $areas['news'][0],
+    	'web' => true
+    ))); ?>
 </div>
 
 <div class="clear"></div>
-<?php if (!empty($adAreas[1])): ?>
-<div id="adAreaIntro" class="adArea">
-	<div id="adAreaIntro" class="adArea">
-	<?php $story = $adAreas[1][0]; // Intentional single story ?>
-		<div class="story" id="story_<?php echo $story->story_id; ?>">
-	        <?php echo $savvy->render($story, 'ENews/Newsletter/Story/Presentation/'.$story->getPresentation()->template); ?>
-        </div>
-	</div>
-</div>
-<?php elseif (!empty($adAreas[2]) || !empty($adAreas[0])): ?>
+<?php if (!empty($areas['ads'][1])): ?>
+    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
+        'id' => 'adAreaIntro',
+        'class' => 'adArea',
+        'stories' => $areas['ads'][1],
+        'filter' => 1, // Forces to only render 1 ad
+    	'web' => true
+    ))); ?>
+<?php elseif (!empty($areas['ads'][2]) || !empty($areas['ads'][2])): ?>
 <div style="width:340px;padding:0 10px 0 0;float:left;">
-	<div id="adArea1" class="adArea">
-	<?php if (!empty($adAreas[2])): ?>
-	<?php $story = $adAreas[2][0]; // Intentional single story ?>
-		<div class="story" id="story_<?php echo $story->story_id; ?>">
-            <?php echo $savvy->render($story, 'ENews/Newsletter/Story/Presentation/'.$story->getPresentation()->template); ?>
-        </div>
-    <?php endif; ?>
-	</div>
+	<?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
+        'id' => 'adArea1',
+        'class' => 'adArea',
+        'stories' => $areas['ads'][2],
+	    'filter' => 1, // Forces to only render 1 ad
+    	'web' => true
+    ))); ?>
 </div>
 <div style="width:340px;padding:0 0 0 10px;float:right;">
-	<div id="adArea2" class="adArea">
-	<?php if (!empty($adAreas[0])): ?>
-	<?php $story = $adAreas[0][0]; // Intentional single story ?>
-		<div class="story" id="story_<?php echo $story->story_id; ?>">
-            <?php echo $savvy->render($story, 'ENews/Newsletter/Story/Presentation/'.$story->getPresentation()->template); ?>
-        </div>
-	<?php endif; ?>
-	</div>
+    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
+        'id' => 'adArea2',
+        'class' => 'adArea',
+        'stories' => $areas['ads'][0],
+	    'filter' => 1, // Forces to only render 1 ad
+    	'web' => true
+    ))); ?>
 </div>
 <?php endif; ?>
