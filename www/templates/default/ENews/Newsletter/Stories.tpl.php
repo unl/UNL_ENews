0c9ecd19038ea3075deb4ffbbc0a8c2f21769ec8
Brett Bieber <brett.bieber@gmail.com>
2011-02-01 01:37:34 +0000

    Whitespace, alignment

--- www/templates/default/ENews/Newsletter/Stories.tpl.php
+++ www/templates/default/ENews/Newsletter/Stories.tpl.php
@@ -6,8 +6,8 @@ $isPreview = $context->getIsPreview();
     <td colspan="3" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
         <!-- This is the main content -->
         <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id' => 'newsColumnIntro',
-            'class' => 'newsColumn',
+            'id'      => 'newsColumnIntro',
+            'class'   => 'newsColumn',
             'stories' => $areas['news'][1],
             'preview' => $isPreview
         ))); ?>
@@ -16,8 +16,8 @@ $isPreview = $context->getIsPreview();
 <tr id="newsStories">
      <td valign="top" width="273">
          <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id' => 'newsColumn1',
-            'class' => 'newsColumn',
+            'id'      => 'newsColumn1',
+            'class'   => 'newsColumn',
             'stories' => $areas['news'][2],
             'preview' => $isPreview
         ))); ?>
@@ -25,8 +25,8 @@ $isPreview = $context->getIsPreview();
     <td width="10">&nbsp;</td>
     <td valign="top" width="273">
         <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id' => 'newsColumn2',
-            'class' => 'newsColumn',
+            'id'      => 'newsColumn2',
+            'class'   => 'newsColumn',
             'stories' => $areas['news'][0],
             'preview' => $isPreview
         ))); ?>
@@ -38,11 +38,11 @@ $isPreview = $context->getIsPreview();
 <tr>
     <td colspan="3" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
         <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id' => 'adAreaIntro',
-            'class' => 'adArea',
+            'id'      => 'adAreaIntro',
+            'class'   => 'adArea',
             'stories' => $areas['ads'][1],
             'preview' => $isPreview,
-            'filter' => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
+            'filter'  => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
         ))); ?>
     </td>
 </tr>
@@ -51,21 +51,21 @@ $isPreview = $context->getIsPreview();
 <tr>
     <td valign="top" style="color:#606060;font-size:12px;line-height:1.4em;font-family:'Lucida Grande',Verdana,Arial;" width="273">
         <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id' => 'adArea1',
-            'class' => 'adArea',
+            'id'      => 'adArea1',
+            'class'   => 'adArea',
             'stories' => $areas['ads'][2],
             'preview' => $isPreview,
-            'filter' => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
+            'filter'  => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
         ))); ?>
     </td>
     <td width="10">&nbsp;</td>
     <td valign="top" style="color:#606060;font-size:12px;line-height:1.4em;font-family:'Lucida Grande',Verdana,Arial;" width="273">
         <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id' => 'adArea2',
-            'class' => 'adArea',
+            'id'      => 'adArea2',
+            'class'   => 'adArea',
             'stories' => $areas['ads'][0],
             'preview' => $isPreview,
-            'filter' => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
+            'filter'  => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
         ))); ?>
     </td>
 </tr>
