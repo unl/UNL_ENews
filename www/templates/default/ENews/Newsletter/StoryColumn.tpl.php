42853af6f396deda0cba5f5b440d4f6b5cc1f460
Brett Bieber <brett.bieber@gmail.com>
2011-02-01 01:46:32 +0000

    Whitespace, tabs to spaces

--- www/templates/default/ENews/Newsletter/StoryColumn.tpl.php
+++ www/templates/default/ENews/Newsletter/StoryColumn.tpl.php
@@ -1,7 +1,7 @@
 <?php /* @var $context UNL_ENews_Newsletter_StoryColumn */ ?>
 <div id="<?php echo $context->getHtmlAttribute('id') ?>" class="<?php echo $context->getHtmlAttribute('class') ?>">
 <?php foreach ($context->getStoriesIterator() as $story): ?>
-	<div class="story" id="story_<?php echo $story->story_id; ?>">
+    <div class="story" id="story_<?php echo $story->story_id; ?>">
     <?php if ($context->isPreview()): ?>
         <div class="story-content">
             <?php echo $savvy->render($story, $story->getRenderer('templates/email')); ?>
@@ -9,11 +9,11 @@
         </div>
         <?php echo $savvy->render($story, 'ENews/Newsletter/Preview/StoryData.tpl.php'); ?>
     <?php elseif ($context->isForWeb()): ?>
-    	<?php echo $savvy->render($story, $story->getRenderer()); ?>
+        <?php echo $savvy->render($story, $story->getRenderer()); ?>
         <div class="clear"></div>
     <?php else: ?>
-    	<?php echo $savvy->render($story, $story->getRenderer()); ?>
-    	<img class="spacer" src="http://www.unl.edu/wdn/templates_3.0/images/email/gif.gif" width="100%" height="10" />
+        <?php echo $savvy->render($story, $story->getRenderer()); ?>
+        <img class="spacer" src="http://www.unl.edu/wdn/templates_3.0/images/email/gif.gif" width="100%" height="10" />
     <?php endif; ?>
     </div>
 <?php endforeach; ?>
