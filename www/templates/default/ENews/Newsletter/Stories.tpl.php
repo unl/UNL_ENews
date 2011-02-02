d5579d877df4085f9183fdb459634b7764e57f8f
Kevin Abel <kabel2@unl.edu>
2011-02-02 18:17:38 +0000

    [#688][#686] Updated the Stories templates to remove much of the code duplication and to remove the portion of the view that was manually instantiating StoryColumn models.

--- www/templates/default/ENews/Newsletter/Stories.tpl.php
+++ www/templates/default/ENews/Newsletter/Stories.tpl.php
@@ -1,76 +1,44 @@
-<?php
-$areas = $context->getStoriesByColumn();
-$isPreview = $context->getIsPreview();
-?>
-<?php if ($isPreview || !empty($areas['news'][1])): ?>
-<tr>
-    <td colspan="3" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
-        <!-- This is the main content -->
-        <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id'      => 'newsColumnIntro',
-            'class'   => 'newsColumn',
-            'stories' => $areas['news'][1],
-            'preview' => $isPreview
-        ))); ?>
-    </td>
-</tr>
-<?php endif; ?>
-<?php if ($isPreview || (!empty($areas['news'][2]) || !empty($areas['news'][0]))): ?>
-<tr id="newsStories">
-     <td valign="top" width="273">
-         <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id'      => 'newsColumn1',
-            'class'   => 'newsColumn',
-            'stories' => $areas['news'][2],
-            'preview' => $isPreview
-        ))); ?>
-    </td>
-    <td width="10">&nbsp;</td>
-    <td valign="top" width="273">
-        <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id'      => 'newsColumn2',
-            'class'   => 'newsColumn',
-            'stories' => $areas['news'][0],
-            'preview' => $isPreview
-        ))); ?>
-    </td>
-</tr>
-<?php endif; ?>
-
-<?php $showAdIntro = $isPreview || !empty($areas['ads'][1]); ?>
-<?php if ($showAdIntro): ?>
-<tr>
-    <td colspan="3" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
-        <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id'      => 'adAreaIntro',
-            'class'   => 'adArea',
-            'stories' => $areas['ads'][1],
-            'preview' => $isPreview,
-            'filter'  => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
-        ))); ?>
-    </td>
-</tr>
-<?php endif; ?>
-<?php if ($isPreview || (!$showAdIntro && (!empty($areas['ads'][2]) || !empty($areas['ads'][0])))): ?>
-<tr>
-    <td valign="top" style="color:#606060;font-size:12px;line-height:1.4em;font-family:'Lucida Grande',Verdana,Arial;" width="273">
-        <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id'      => 'adArea1',
-            'class'   => 'adArea',
-            'stories' => $areas['ads'][2],
-            'preview' => $isPreview,
-            'filter'  => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
-        ))); ?>
-    </td>
-    <td width="10">&nbsp;</td>
-    <td valign="top" style="color:#606060;font-size:12px;line-height:1.4em;font-family:'Lucida Grande',Verdana,Arial;" width="273">
-        <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-            'id'      => 'adArea2',
-            'class'   => 'adArea',
-            'stories' => $areas['ads'][0],
-            'preview' => $isPreview,
-            'filter'  => ($isPreview ? -1 : 1) // Forces the non-preview to only render 1 ad
-        ))); ?>
-    </td>
-</tr>
-<?php endif; ?>
+<?php $isPreview = $context->getIsPreview(); ?>
+<?php foreach (array('news', 'ads') as $area): ?>
+    <?php
+    $stories = $context->getStoriesByColumn($area);
+    $displayColumn = $isPreview || !empty($stories[1]);
+    ?>
+    <?php if ($displayColumn): ?>
+    <tr>
+        <td colspan="3" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
+            <?php echo $savvy->render($context->getStoryColumn($stories[1], array(
+                'area' => $area,
+                'offset' => 1,
+                'preview' => $isPreview
+            ))); ?>
+        </td>
+    </tr>
+    <?php endif; ?>
+    <?php
+    if ($area == 'news') {
+        $displayColumn = $isPreview || (!empty($stories[2]) || !empty($stories[0]));
+    } else {
+        $displayColumn = $isPreview || (!$displayColumn && (!empty($stories[2]) || !empty($stories[0])));
+    }
+    ?>
+    <?php if ($displayColumn): ?>
+    <tr>
+         <td valign="top" width="273">
+             <?php echo $savvy->render($context->getStoryColumn($stories[2], array(
+                'area' => $area,
+                'offset' => 2,
+                'preview' => $isPreview
+            ))); ?>
+        </td>
+        <td width="10">&nbsp;</td>
+        <td valign="top" width="273">
+            <?php echo $savvy->render($context->getStoryColumn($stories[0], array(
+                'area' => $area,
+                'offset' => 0,
+                'preview' => $isPreview
+            ))); ?>
+        </td>
+    </tr>
+    <?php endif; ?>
+<?php endforeach; ?>
--- www/templates/default/ENews/Newsletter/StoriesWeb.tpl.php
+++ www/templates/default/ENews/Newsletter/StoriesWeb.tpl.php
@@ -1,59 +1,38 @@
-<?php $areas = $context->getStoriesByColumn(); ?>
+<?php foreach (array('news', 'ads') as $area): ?>
+	<?php
+    $stories = $context->getStoriesByColumn($area);
+    $displayColumn = !empty($stories[1]);
+    ?>
+    <?php if ($displayColumn): ?>
+    <?php echo $savvy->render($context->getStoryColumn($stories[1], array(
+        'area' => $area,
+        'offset' => 1,
+        'web' => true
+    ))); ?>
+    <?php endif; ?>
+    <?php
+    if ($area == 'news') {
+        $displayColumn = !empty($stories[2]) || !empty($stories[0]);
+    } else {
+        $displayColumn = !$displayColumn && (!empty($stories[2]) || !empty($stories[0]));
+    }
+    ?>
+    <?php if ($displayColumn): ?>
+    <div style="width:340px;padding:0 10px 0 0;float:left;">
+        <?php echo $savvy->render($context->getStoryColumn($stories[2], array(
+            'area' => $area,
+            'offset' => 2,
+            'web' => true
+        ))); ?>
+    </div>
+    <div style="width:340px;padding:0 0 0 10px;float:right;">
+        <?php echo $savvy->render($context->getStoryColumn($stories[0], array(
+            'area' => $area,
+            'offset' => 0,
+            'web' => true
+        ))); ?>
+    </div>
+    <?php endif; ?>
 
-<?php if (!empty($areas['news'][1])): ?>
-<?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-    'id' => 'newsColumnIntro',
-    'class' => 'newsColumn',
-    'stories' => $areas['news'][1],
-    'web' => true
-))); ?>
-<?php endif; ?>
-
-<?php if (!empty($areas['news'][2]) || !empty($areas['news'][0])): ?>
-<div style="width:340px;padding:0 10px 0 0;float:left;">
-    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-        'id' => 'newsColumn1',
-        'class' => 'newsColumn',
-        'stories' => $areas['news'][2],
-    	'web' => true
-    ))); ?>
-</div>
-<div style="width:340px;padding:0 0 0 10px;float:right;">
-    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-        'id' => 'newsColumn2',
-        'class' => 'newsColumn',
-        'stories' => $areas['news'][0],
-    	'web' => true
-    ))); ?>
-</div>
-<?php endif; ?>
-
-<div class="clear"></div>
-<?php if (!empty($areas['ads'][1])): ?>
-    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-        'id' => 'adAreaIntro',
-        'class' => 'adArea',
-        'stories' => $areas['ads'][1],
-        'filter' => 1, // Forces to only render 1 ad
-    	'web' => true
-    ))); ?>
-<?php elseif (!empty($areas['ads'][2]) || !empty($areas['ads'][0])): ?>
-<div style="width:340px;padding:0 10px 0 0;float:left;">
-	<?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-        'id' => 'adArea1',
-        'class' => 'adArea',
-        'stories' => $areas['ads'][2],
-	    'filter' => 1, // Forces to only render 1 ad
-    	'web' => true
-    ))); ?>
-</div>
-<div style="width:340px;padding:0 0 0 10px;float:right;">
-    <?php echo $savvy->render(new UNL_ENews_Newsletter_StoryColumn(array(
-        'id' => 'adArea2',
-        'class' => 'adArea',
-        'stories' => $areas['ads'][0],
-	    'filter' => 1, // Forces to only render 1 ad
-    	'web' => true
-    ))); ?>
-</div>
-<?php endif; ?>
+    <div class="clear"></div>
+<?php endforeach; ?>
