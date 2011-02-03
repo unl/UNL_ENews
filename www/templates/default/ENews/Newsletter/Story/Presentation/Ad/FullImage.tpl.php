925578bbea0be3a8f0d5be1923743e8a32a1074e
Kevin Abel <kabel2@unl.edu>
2011-02-03 21:37:34 +0000

    Updated the FullImage Ad presentation to use the correct width AND height when rendering img. (Default and Email are nearly identical)

--- www/templates/default/ENews/Newsletter/Story/Presentation/Ad/FullImage.tpl.php
+++ www/templates/default/ENews/Newsletter/Story/Presentation/Ad/FullImage.tpl.php
@@ -3,15 +3,21 @@ if (!$context->hasNotExpired()) {
     return;
 }
 
-$width = $context->getColFromSort() == 'twocol' ? 680 : 320;
+if ($context->getColFromSort() == 'twocol') {
+    $width = UNL_ENews_File_Image::FULL_AD_WIDTH;
+    $height = UNL_ENews_File_Image::FULL_AD_HEIGHT;
+} else {
+    $width = UNL_ENews_File_Image::HALF_AD_WIDTH;
+    $height = UNL_ENews_File_Image::HALF_AD_HEIGHT;
+}
 
 if ($file = $context->getFileByUse('originalimage')) {
-    echo '<div style="background:#f4f2cc;padding:10px">';
+    echo '<div style="background:#f4f2cc;padding:10px;margin-top:10px;margin-bottom:10px;">';
     if (!empty($context->website)) {
         echo '<a href="'.$context->website.'">';
     }
 
-    echo '<img src="'.$file->getURL().'" style="width:'. $width .'px;margin-bottom:5px;" />';
+    echo '<img src="'.$file->getURL().'" style="width:'. $width .'px;height:'. $height .'px;" />';
 
     if (!empty($context->website)) {
         echo '</a>';
