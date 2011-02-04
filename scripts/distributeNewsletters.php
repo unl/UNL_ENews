26c06dd1be3de4a7b2c7737dd895da45818d9ba6
Brett Bieber <brett.bieber@gmail.com>
2011-02-04 14:42:50 +0000

    Change to the web directory with the templates since we're apparently not using the template path for all references

--- scripts/distributeNewsletters.php
+++ scripts/distributeNewsletters.php
@@ -7,6 +7,8 @@ if (file_exists(dirname(__FILE__).'/../www/config.inc.php')) {
     require dirname(__FILE__).'/../www/config.sample.php';
 }
 
+chdir(dirname(__FILE__).'/../www');
+
 foreach (UNL_ENews_NewsletterList::getUndistributed() as $newsletter) {
     $newsletter->distribute();
     $newsletter->distributed = 1;
