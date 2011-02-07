79ddd9737731d95daa6689f41ec795a2153e4197
Eric Rasmussen <erasmussen2@unl.edu>
2011-02-07 20:09:54 +0000

    Don't show ads in the sidebar list of stories (in story view)

--- www/templates/default/ENews/Newsletter/Story.tpl.php
+++ www/templates/default/ENews/Newsletter/Story.tpl.php
@@ -6,26 +6,28 @@ $newsletter_url = $context->newsletter->getURL();
 </div>
 <div class="col right">
     <div class="sidebar top">
-    	<div class="inner_sidebar">
-    		<h3>
-	            <a href="<?php echo $context->getURL(); ?>" title="Go to the newsletter index page"><?php echo $context->newsroom->name; ?>
-	            <span class="date">
-	            <?php echo date('D. M d, Y', strtotime($context->newsletter->release_date)) ?>
-	            </span>
-	            </a>
-	        </h3>
-	        <ul>
-		    <?php 
-		    foreach ($context->newsletter->getStories() as $key=>$story) {
-		        echo '<li><a href="'.$newsletter_url.'/'.$story->id.'">'.$story->title.'</a></li>';
-		    }
-		    ?>
-		    </ul>
-	    </div>
-	</div>
-	<div class="sidebar bottom">
-	    <div class="inner_sidebar">
-	        <?php echo $savvy->render($context->newsroom, 'ENews/Newsroom/SubscribeForm.tpl.php'); ?>
-	    </div>
+      <div class="inner_sidebar">
+        <h3>
+              <a href="<?php echo $context->getURL(); ?>" title="Go to the newsletter index page"><?php echo $context->newsroom->name; ?>
+              <span class="date">
+              <?php echo date('D. M d, Y', strtotime($context->newsletter->release_date)) ?>
+              </span>
+              </a>
+          </h3>
+          <ul>
+        <?php 
+        foreach ($context->newsletter->getStories() as $key => $story) {
+            if ($story->presentation->type != 'ad') {
+                echo '<li><a href="'.$newsletter_url.'/'.$story->id.'">'.$story->title.'</a></li>';
+            }
+        }
+        ?>
+        </ul>
+      </div>
+  </div>
+  <div class="sidebar bottom">
+      <div class="inner_sidebar">
+          <?php echo $savvy->render($context->newsroom, 'ENews/Newsroom/SubscribeForm.tpl.php'); ?>
+      </div>
     </div>
 </div>
