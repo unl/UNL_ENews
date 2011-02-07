90615ac2ca44ceab5a4e620dc174a1d234485727
Eric Rasmussen <erasmussen2@unl.edu>
2011-02-07 20:24:20 +0000

    Only show the "Subscribe Now!" headline if there is an email address available to subscribe to

--- www/templates/default/ENews/Newsroom/SubscribeForm.tpl.php
+++ www/templates/default/ENews/Newsroom/SubscribeForm.tpl.php
@@ -1,8 +1,11 @@
-<div class="subscribe">
-    <h3><?php echo $context->name; ?><span>Subscribe Today!</span></h3>
-    <?php foreach ($context->getEmails() as $email) :
+<?php
+$subscribeBoxSet = false;
+foreach ($context->getEmails() as $email) :
     if ($email->optout) :
-        ?>
+        if (!$subscribeBoxSet) : ?>
+        <div class="subscribe">
+        <h3><?php echo $context->name; ?><span>Subscribe Today!</span></h3>
+        <?php endif; ?>
         <form method="get" action="http://listserv.unl.edu/signup-anon/" id="subscribe">
             <label for="address">Email</label>
             <input type="text" id="address" name="ADDRESS" value="" />
@@ -11,7 +14,9 @@
             <input type="hidden" name="LISTNAME" value="<?php echo substr($email->email, 0, strpos($email->email, '@')); ?>" />
             <input type="submit" value="Subscribe" name="submit" />
         </form>
-    <?php 
+        <?php if (!$subscribeBoxSet) : ?>
+        </div>
+        <?php $subscribeBoxSet = true;
         endif;
-    endforeach; ?>
-</div>
+    endif;
+endforeach;
