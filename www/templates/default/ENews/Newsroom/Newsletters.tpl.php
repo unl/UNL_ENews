c86b0be3c673e73010b38c0a15082aa3bfa78a6d
Brett Bieber <brett.bieber@gmail.com>
2011-03-10 15:59:43 +0000

    Add newsletter date to the list of newsletters so you can easily identify which day they're scheduled to be sent. Change "Send" button text to "Distribute Now" and add more warning text.

--- www/templates/default/ENews/Newsroom/Newsletters.tpl.php
+++ www/templates/default/ENews/Newsroom/Newsletters.tpl.php
@@ -16,7 +16,7 @@ WDN.jQuery('document').ready(function(){
             $newsletterDate = strtotime($newsletter->release_date);
         ?>
         <tr>
-            <td class="mainCell" style="min-width:600px;"><h5><?php echo $newsletter->subject; ?></h5>
+            <td class="mainCell" style="min-width:600px;"><h5><?php echo $newsletter->subject; ?> <span class="caption">(<?php echo date('D. M d, Y', $newsletterDate); ?>)</span></h5>
                 <a href="?view=preview&amp;id=<?php echo $newsletter->id; ?>" class="action edit">Edit</a>
                 <?php echo $savvy->render($newsletter, 'ENews/Newsletter/SendPreviewForm.tpl.php'); ?>
                 <form action="?view=newsletters" method="post" id="deletenewsletter_<?php echo $newsletter->id; ?>" style="width:120px;">
@@ -30,7 +30,7 @@ WDN.jQuery('document').ready(function(){
                 <?php if ($newsletter->distributed): ?>
                 <strong>Sent:</strong> <?php echo date('D. M d, Y', $newsletterDate); ?>
                 <?php else: ?>
-                <a class="action send" href="?view=sendnews&amp;id=<?php echo $newsletter->id; ?>" onclick="return confirm('Are you sure?')">Send</a>
+                <a class="action send" href="?view=sendnews&amp;id=<?php echo $newsletter->id; ?>" onclick="return confirm('This newsletter is scheduled for distribution on <?php echo date('M jS', $newsletterDate); ?>.\n\nAre you sure you want to send it now?')">Distribute Now</a>
                 <?php endif; ?>
             </td>
             <td align="center">
