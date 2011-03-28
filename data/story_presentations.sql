fb8624bb66e001923731533f7c3d22183bf1e4b2
Kevin Abel <kabel2@unl.edu>
2011-03-28 22:33:42 +0000

    Added a new story presentation for event stories. Made comment note that that update.php considers ID 7 reserved for an invalid presentation. Requires update.php run.

--- data/story_presentations.sql
+++ data/story_presentations.sql
@@ -4,5 +4,7 @@ INSERT INTO `story_presentations` (`id`, `type`, `isdefault`, `description`, `te
 (3, 'news', 0, 'Full-Width image above text', 'News/FullColImage.tpl.php'),
 (4, 'event', 1, 'Thumbnail floated left', 'Event/ThumbnailFloatedLeft.tpl.php'),
 (5, 'event', 0, 'Thumbnail floated right', 'Event/ThumbnailFloatedRight.tpl.php'),
-(6, 'ad', 1, 'Full-Width image only', 'Ad/FullImage.tpl.php')
+(6, 'ad', 1, 'Full-Width image only', 'Ad/FullImage.tpl.php'),
+-- 7 was reserved for an invalid ad presentation
+(8, 'event', 0, 'Full-Width image above text', 'Event/FullColImage.tpl.php')
     ON DUPLICATE KEY UPDATE template=VALUES(template), type=VALUES(type), isdefault=VALUES(isdefault), description=VALUES(description);
