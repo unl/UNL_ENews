a782c7a0ef3e1bee1fc7a6da774aa0209cd1d663
Brett Bieber <brett.bieber@gmail.com>
2011-04-21 20:06:18 +0000

    Send http status code to the client on error (if it was set).

--- www/templates/default/Exception.tpl.php
+++ www/templates/default/Exception.tpl.php
@@ -1,3 +1,12 @@
+<?php
+/* @var $context Exception */
+if (false == headers_sent()
+    && $code = $context->getCode()) {
+    header('HTTP/1.1 '.$code.' '.$context->getMessage());
+    header('Status: '.$code.' '.$context->getMessage());
+}
+?>
+
 <script type="text/javascript">
 WDN.initializePlugin('notice');
 </script>
