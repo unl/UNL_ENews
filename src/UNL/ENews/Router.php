425ef31dbf25a4d19411936ca7670d5cb7970f99
Brett Bieber <brett.bieber@gmail.com>
2011-06-15 15:17:20 +0000

    Major cleanup for readability. Remove the quoted base and trim URL so we're only working on the relevant parts of the path.

--- src/UNL/ENews/Router.php
+++ src/UNL/ENews/Router.php
@@ -4,57 +4,57 @@ class UNL_ENews_Router
     public static function route($requestURI)
     {
 
-        $base = UNL_ENews_Controller::getURL();
-
-        if (filter_var($base, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED)) {
-            $base = parse_url($base, PHP_URL_PATH);
-        }
-
-        $quotedBase = preg_quote($base, '/');
-
         if (!empty($_SERVER['QUERY_STRING'])) {
             $requestURI = substr($requestURI, 0, -strlen(urldecode($_SERVER['QUERY_STRING'])) - 1);
         }
 
+        // Trim the base part of the URL
+        $requestURI = substr($requestURI, strlen(parse_url(UNL_ENews_Controller::getURL(), PHP_URL_PATH)));
         $options = array();
+
+
+        if (empty($requestURI)) {
+            // Default view/homepage
+            return $options;
+        }
 
         switch(true) {
             // Files associated with tstories
-            case preg_match('/'.$quotedBase.'file([\d]+)\.(jpg|png|gif)$/', $requestURI, $matches):
+            case preg_match('/^file([\d]+)\.(jpg|png|gif)$/', $requestURI, $matches):
                 $options['view']         = 'file';
                 $options['id']           = $matches[1];
                 $options['content-type'] = $matches[2];
                 break;
             //For calling a story.  url = www/newsRoomShortName/newsletterID/storyID/
-            case preg_match('/'.$quotedBase.'([a-z]+)\/([\d]+)\/([\d]+)$/i', $requestURI, $matches):
+            case preg_match('/^([a-z]+)\/([\d]+)\/([\d]+)$/i', $requestURI, $matches):
                 $options['view']          = 'newsletterStory';
                 $options['shortname']     = $matches[1];
                 $options['newsletter_id'] = $matches[2];
                 $options['id']            = $matches[3];
                 break;
             //For calling a newsletter.  url = www/newsRoomShortName/newsletterID/
-            case preg_match('/'.$quotedBase.'([a-z]+)\/([\d]+)\/?$/i', $requestURI, $matches):
+            case preg_match('/^([a-z]+)\/([\d]+)\/?$/i', $requestURI, $matches):
                 $options['view']         = 'newsletter';
                 $options['shortname']    = $matches[1];
                 $options['id']           = $matches[2];
                 break;
             //For submiting to a news letter.  url = www/newsRoomShortName/submit
-            case preg_match('/'.$quotedBase.'([a-z]+)\/submit$/i', $requestURI, $matches):
+            case preg_match('/^([a-z]+)\/submit$/i', $requestURI, $matches):
                 $options['view']         = 'submit';
                 $options['shortname']    = $matches[1];
                 break;
             // For managing a newsroom.
-            case preg_match('/'.$quotedBase.'([a-z]+)\/manage$/i', $requestURI, $matches):
+            case preg_match('/^([a-z]+)\/manage$/i', $requestURI, $matches):
                 $options['view']         = 'manager';
                 $options['shortname']    = $matches[1];
                 break;
             //For viewing the newest newsletter for a newsroom.
-            case preg_match('/'.$quotedBase.'([a-z]+)\/?$/i', $requestURI, $matches):
+            case preg_match('/^([a-z]+)\/?$/i', $requestURI, $matches):
                 $options['view']         = 'newsletter';
                 $options['shortname']    = $matches[1];
                 break;
             //For viewing an archive.  url = www/newsRoomShortName/archive
-            case preg_match('/'.$quotedBase.'([a-z]+)\/archive$/i', $requestURI, $matches):
+            case preg_match('/^([a-z]+)\/archive$/i', $requestURI, $matches):
                 $options['view']         = 'archive';
                 $options['shortname']    = $matches[1];
                 break;
