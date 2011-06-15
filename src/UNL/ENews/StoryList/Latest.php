f8515f3ce83444255a8b102d1f02f3e41b05a167
Brett Bieber <brett.bieber@gmail.com>
2011-06-15 21:26:43 +0000

    Only show stories that are within their current "publish range"

--- src/UNL/ENews/StoryList/Latest.php
+++ src/UNL/ENews/StoryList/Latest.php
@@ -10,13 +10,23 @@ class UNL_ENews_StoryList_Latest extends UNL_ENews_StoryList
     
     function __construct($options = array())
     {
-        if (!$this->newsroom = UNL_ENews_Newsroom::getByID($options['newsroom'])) {
+        if (!$this->newsroom = UNL_ENews_Newsroom::getByOptions($options)) {
             throw new Exception('Newsroom not found', 404);
         }
-        
+
         $stories = array();
         $mysqli = UNL_ENews_Controller::getDB();
-        $sql = 'SELECT story_id FROM newsroom_stories WHERE status != "pending" AND newsroom_id = '.(int)$options['newsroom'];
+        $sql = '
+        	SELECT 
+        		newsroom_stories.story_id 
+        	FROM newsroom_stories, stories
+        	WHERE
+        		newsroom_stories.story_id = stories.id
+        			AND newsroom_stories.status != "pending"
+        			AND newsroom_stories.newsroom_id = '.(int)$this->newsroom->id. '
+        			AND "'.date('Y-m-d H:i:s').'" BETWEEN stories.request_publish_start
+        			AND stories.request_publish_end
+        	ORDER BY stories.request_publish_start DESC';
         if ($result = $mysqli->query($sql)) {
             while($row = $result->fetch_array(MYSQLI_NUM)) {
                 $stories[] = $row[0];
