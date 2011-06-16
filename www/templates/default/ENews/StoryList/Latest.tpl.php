ed585b8a6a7db3483f706bc69f70aca58535744f
Brett Bieber <brett.bieber@gmail.com>
2011-06-16 18:52:00 +0000

    Minor cleanup to the story list

--- www/templates/default/ENews/StoryList/Latest.tpl.php
+++ www/templates/default/ENews/StoryList/Latest.tpl.php
@@ -2,13 +2,14 @@
     <h3 class="sec_main">
         <?php echo $context->newsroom->name;?><a class="rsslink right" href="<?php echo $context->newsroom->getURL(); ?>/latest?format=rss">RSS</a>
     </h3>
-    <ul>
-        <?php foreach($context as $item) :?>
-        <li style="clear:both"><?php echo $savvy->render($item); ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="stories">
+        <?php
+        foreach($context as $item) {
+            echo $savvy->render($item, 'ENews/Newsletter/Story/Summary.tpl.php');
+        }
+        ?>
+    </div>
 </div>
 <div class="col right">
     <a href="<?php echo $context->newsroom->getSubmitURL(); ?>">Submit News</a>
-    <br />
 </div>
