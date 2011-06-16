7b1077d00715e4c324d8e0b92d4e2b2fc148287c
Brett Bieber <brett.bieber@gmail.com>
2011-06-16 18:51:38 +0000

    Improve summary. Use a specific format for the story summary instead of re-using the default news presentation format.

--- www/templates/default/ENews/Newsletter/Story/Summary.tpl.php
+++ www/templates/default/ENews/Newsletter/Story/Summary.tpl.php
@@ -1,3 +1,17 @@
-<?php
-echo $savvy->render($context, $context->getRenderer());
-?>
+<?php $storylink = $context->getURL();?>
+<div class="article">
+	<h4><a href="<?=echo $storylink ?>" title="Read more about this story"><?php echo $context->title; ?></a></h4>
+	<?php
+    if ($file = $context->getThumbnail()) {
+        $description = $file->name;
+        if (!empty($file->description)) {
+            $description = $file->description;
+        }
+        echo   '<a href="'.$storylink.'">'
+             . '<img src="'.$file->getURL(). '" alt="'.$description.'" />'
+             . '</a>';
+    }
+    ?>
+
+	<p><?php echo nl2br($context->description); ?></p>
+</div>
