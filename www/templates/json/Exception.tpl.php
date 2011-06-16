75b753dd13fdb19ddffe4642d477ac47385733b9
Brett Bieber <brett.bieber@gmail.com>
2011-06-16 17:07:05 +0000

    Add error headers to the exception output

--- www/templates/json/Exception.tpl.php
+++ www/templates/json/Exception.tpl.php
@@ -1,1 +1,9 @@
+<?php
+/* @var $context Exception */
+if (false == headers_sent()
+    && $code = $context->getCode()) {
+    header('HTTP/1.1 '.$code.' '.$context->getMessage());
+    header('Status: '.$code.' '.$context->getMessage());
+}
+?>
 "error":<?php echo json_encode((string)$context); ?>
