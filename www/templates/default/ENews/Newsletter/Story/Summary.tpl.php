d55d8d954cf8bc3142de796a5c2b46c56b71ec01
Brett Bieber <brett.bieber@gmail.com>
2011-06-21 21:16:28 +0000

    Move image before heading

--- www/templates/default/ENews/Newsletter/Story/Summary.tpl.php
+++ www/templates/default/ENews/Newsletter/Story/Summary.tpl.php
@@ -1,6 +1,5 @@
 <?php $storylink = $context->getURL();?>
 <div class="article">
-	<h4><a href="<?php echo $storylink; ?>" title="Read more about this story"><?php echo $context->title; ?></a></h4>
 	<?php
 	/* @var $context UNL_ENews_Story_Summary */
 	$image_type = 'thumbnail';
@@ -12,11 +11,12 @@
         if (!empty($file->description)) {
             $description = $file->description;
         }
-        echo   '<a href="'.$storylink.'">'
+        echo   '<a href="'.$storylink.'" title="Read more about '.$context->title.'">'
              . '<img src="'.$file->getURL(). '" alt="'.$description.'" />'
              . '</a>';
     }
     ?>
+	<h4><a href="<?php echo $storylink; ?>" title="Read more about <?php echo $context->title; ?>"><?php echo $context->title; ?></a></h4>
 
 	<p><?php echo nl2br($context->description); ?></p>
 </div>
