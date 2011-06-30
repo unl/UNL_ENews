9e06209cb52e0ecf63bd295b52541c93f95b03cf
Brett Bieber <brett.bieber@gmail.com>
2011-06-30 19:32:41 +0000

    Use the helper methods to improve checks for whether a story has been published or not.

--- src/UNL/ENews/PublishedStory.php
+++ src/UNL/ENews/PublishedStory.php
@@ -4,8 +4,31 @@ class UNL_ENews_PublishedStory extends UNL_ENews_Story
     function __construct($options = array())
     {
         parent::__construct($options);
-        if (strtotime($this->request_publish_start) < time()) {
-            throw new Exception('This story has not been published yet!');
+        $this->checkPreconditions();
+    }
+
+    function checkPreconditions()
+    {
+        // Check if there is a user logged in that can edit this story
+        if ($user = UNL_ENews_Controller::getUser()) {
+            if ($this->userCanEdit($user)) {
+                return true;
+            }
         }
+
+        // No user or user cannot edit. Verify the publish date.
+
+        if (false === $this->pastPublishStart()) {
+            throw new Exception('That story is not available to the public yet.', 400);
+        }
+
+        // We must be past the start date
+        if ($this->getTypeString() == 'ad'
+            && false === $this->hasNotExpired()) {
+            throw new Exception('That advertisement is no longer available to the public.', 400);
+        }
+
+        // We're past the publish date, and it's not an ad so the content is still available
+        return true;
     }
 }
