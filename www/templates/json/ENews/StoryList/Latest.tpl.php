3eecf65929a59003a977fd4162c8c17290ecb44a
Brett Bieber <brett.bieber@gmail.com>
2011-08-03 18:42:40 +0000

    Add URL to files, and individual stories.

--- www/templates/json/ENews/StoryList/Latest.tpl.php
+++ www/templates/json/ENews/StoryList/Latest.tpl.php
@@ -2,7 +2,16 @@
 <?php
 $stories = array();
 foreach ($context as $story) {
-    $stories[] = $story->toArray();
+    /* @var $story UNL_ENews_Story */
+    $story_data = $story->toArray();
+    $files_data = array();
+    foreach ($story->getFiles() as $id=>$file) {
+        $file_data = $file->toArray();
+        unset($file_data['data']);
+        $files_data[$file->getURL()] = $file_data;
+    }
+    $story_data['files'] = $files_data;
+    $stories[$story->getURL()] = $story_data;
 }
 echo json_encode($stories);
 ?>
