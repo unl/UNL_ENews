2e7fa87d9f11823726710acccb8b47613ed3aebd
Brett Bieber <brett.bieber@gmail.com>
2011-08-05 15:18:00 +0000

    Revert to previous revisions. Older Lotus Notes clients cannot render png images, and it is better to have a small red x than a huge empty box.

--- www/templates/email/ENews/Newsletter/Story/Presentation/News/FullTextFullColImage.tpl.php
+++ www/templates/email/ENews/Newsletter/Story/Presentation/News/FullTextFullColImage.tpl.php
@@ -5,14 +5,11 @@ if ($context->getColFromSort() == 'onecol') {
 }
 ?>
 
-<?php if ($file = $context->getFileByUse($use, true)):
-/* @var $file UNL_ENews_File_Image */
-$size = $file->getSize();
-?>
+<?php if ($file = $context->getFileByUse($use, true)): ?>
 <table cellspacing="0" cellpadding="0" border="0" width="100%">
 <tr>
 <td align="left">
-<img src="<?php echo $file->getURL(); ?>"  width="<?php echo $size[0]; ?>" height="<?php echo $size[1]; ?>" style="margin-bottom:5px;width:100%;" />
+<img src="<?php echo $file->getURL(); ?>" style="margin-bottom:5px;width:100%;" />
 </td>
 </tr>
 </table>
