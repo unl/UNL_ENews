72328801e7e51ade0d07f2fe8cdceeca85a1c0ef
Brett Bieber <brett.bieber@gmail.com>
2011-08-23 20:15:46 +0000

    Add output template for published news stories

---
+++ www/templates/rss/ENews/Newsroom/Stories/Published.tpl.php
@@ -0,0 +1,15 @@
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+        <title><?php echo $context->newsroom->name; ?></title>
+        <link><?php echo UNL_ENews_Controller::getURL(); ?></link>
+        <description>Published news from <?php echo $context->newsroom->name; ?></description>
+        <language>en-us</language>
+        <generator>Magic</generator>
+        <lastBuildDate><?php echo date('r'); ?></lastBuildDate>
+        <atom:link href="<?php echo $context->newsroom->getURL(); ?>/stories?format=xml" rel="self" type="application/rss+xml" />
+        <?php foreach($context as $item) :?>
+            <?php echo $savvy->render($item, 'ENews/Story.tpl.php'); ?>
+        <?php endforeach; ?>
+
+    </channel>
+</rss>
