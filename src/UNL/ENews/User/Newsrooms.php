81a8787299f0f1c121173436f766b1f9af73665e
Brett Bieber <brett.bieber@gmail.com>
2011-09-03 13:18:39 +0000

    Whitespace tabs to spaces

--- src/UNL/ENews/User/Newsrooms.php
+++ src/UNL/ENews/User/Newsrooms.php
@@ -4,11 +4,11 @@ class UNL_ENews_User_Newsrooms extends UNL_ENews_NewsroomList
     function __construct($options = array())
     {
         $newsroom_ids = array();
-    	$mysqli = UNL_ENews_Controller::getDB();
+        $mysqli = UNL_ENews_Controller::getDB();
         $sql = "SELECT * FROM user_has_permission WHERE user_uid = '".$mysqli->escape_string($options['uid'])."'";
         if ($result = $mysqli->query($sql)) {
             while ($row = $result->fetch_assoc()) {
-            	$newsroom_ids[] = $row['newsroom_id'];
+                $newsroom_ids[] = $row['newsroom_id'];
             }
         }
         parent::__construct($newsroom_ids);
