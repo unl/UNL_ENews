9dbea366ce82e68c7b4728f27a86ef43c10267f1
Brett Bieber <brett.bieber@gmail.com>
2011-09-21 16:44:31 +0000

    Add support for newsletter RSS feeds.

---
+++ www/templates/rss/ENews/Newsletter.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $context UNL_ENews_Newsletter */
+?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+        <title><?php echo $context->subject; ?></title>
+        <link><?php echo $context->getURL(); ?></link>
+        <description>Stories from the <?php echo $context->newsroom->name; ?> newsletter</description>
+        <language>en-us</language>
+        <generator>UNL_ENews</generator>
+        <lastBuildDate><?php echo date('r', strtotime($context->release_date)); ?></lastBuildDate>
+        <atom:link href="<?php echo $context->getURL(); ?>?format=rss" rel="self" type="application/rss+xml" />
+        <?php foreach($context->getStories() as $item) :?>
+            <?php echo $savvy->render($item, 'ENews/Story.tpl.php'); ?>
+        <?php endforeach; ?>
+    </channel>
+</rss>
+
