d5f2f62278abd3052b4040605e220cae36a0a2a3
Brett Bieber <brett.bieber@gmail.com>
2011-10-21 13:44:32 +0000

    Documentation

--- scripts/distributeNewsletters.php
+++ scripts/distributeNewsletters.php
@@ -1,5 +1,8 @@
 #!/usr/bin/env php
 <?php
+/**
+ * Distribute newsletters that are both undistributed and past their send date
+ */
 
 if (file_exists(dirname(__FILE__).'/../www/config.inc.php')) {
     require_once dirname(__FILE__).'/../www/config.inc.php';
@@ -10,6 +13,7 @@ if (file_exists(dirname(__FILE__).'/../www/config.inc.php')) {
 chdir(dirname(__FILE__).'/../www');
 
 foreach (UNL_ENews_NewsletterList::getUndistributed() as $newsletter) {
+    /* @var $newsletter UNL_ENews_Newsletter */
     $newsletter->distribute();
     $newsletter->distributed = 1;
     $newsletter->save();
