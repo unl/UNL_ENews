b19b86e6cc10416ed245eaeba2fae71fbef09118
Kevin Abel <kevin.abel.0@gmail.com>
2011-11-18 11:53:35 -0600

    Use a form to send a distribution.

--- www/templates/html/ENews/Newsroom/Newsletters.tpl.php
+++ www/templates/html/ENews/Newsroom/Newsletters.tpl.php
@@ -24,13 +24,16 @@ WDN.jQuery('document').ready(function(){
                     <input type="hidden" name="newsletter_id" value="<?php echo $newsletter->id; ?>" />
                     <a href="#" onclick="if (confirm('Are you sure?')) document.getElementById('deletenewsletter_<?php echo $newsletter->id; ?>').submit();">Delete</a>
                 </form>
-                
+
             </td>
             <td align="center">
                 <?php if ($newsletter->distributed): ?>
                 <strong>Sent:</strong> <?php echo date('D. M d, Y', $newsletterDate); ?>
                 <?php else: ?>
-                <a class="action send" href="<?php echo UNL_ENews_Controller::getURL(); ?>?view=sendnews&amp;id=<?php echo $newsletter->id; ?>" onclick="return confirm('This newsletter is scheduled for distribution on <?php echo date('M jS', $newsletterDate); ?>.\n\nAre you sure you want to send it now?')">Distribute Now</a>
+                <form action="<?php echo UNL_ENews_Controller::getURL(); ?>?view=sendnews&amp;id=<?php echo $newsletter->id; ?>" method="post" id="sendnewsletter_<?php echo $newsletter->id; ?>">
+                    <input type="hidden" name="newsletter_id" value="<?php echo $newsletter->id; ?>" />
+                    <a class="action send" href="#" onclick="if (confirm('This newsletter is scheduled for distribution on <?php echo date('M jS', $newsletterDate); ?>.\n\nAre you sure you want to send it now?')) document.getElementById('sendnewsletter_<?php echo $newsletter->id; ?>').submit();">Distribute Now</a>
+                </form>
                 <?php endif; ?>
             </td>
             <td align="center">
