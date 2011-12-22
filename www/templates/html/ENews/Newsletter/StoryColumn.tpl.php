05138eabde4fec42d63e4cd7da43addfc843ce11
Brett Bieber <brett.bieber@gmail.com>
2011-12-22 10:49:26 -0600

    Add method to determine preferred width and use this for spacer gif #14

--- www/templates/html/ENews/Newsletter/StoryColumn.tpl.php
+++ www/templates/html/ENews/Newsletter/StoryColumn.tpl.php
@@ -12,7 +12,13 @@
         <?php echo $savvy->render($story, $story->getRenderer()); ?>
         <div class="clear"></div>
     <?php else: ?>
-        <?php echo $savvy->render($story, $story->getRenderer()); ?>
+        <?php
+        echo $savvy->render($story, $story->getRenderer());
+        $spacer_width  = $story->getWidthFromSortOrder();
+        $spacer_url    = UNL_ENews_Controller::getURL() . 'templates/email/ENews/images/'.$spacer_width.'_10.gif';
+        $spacer_height = 10;
+        ?>
+        <img class="spacer" src="<?php echo $spacer_url; ?>" width="<?php echo $spacer_width; ?>" height="10" />
     <?php endif; ?>
     </article>
 <?php endforeach; ?>
