2ce8519d5cf46c4e89b7bee23776eb2772f29e05
Eric Rasmussen <ericrasmussen1@gmail.com>
2016-07-19 15:09:31 -0500

    Initial move of email to 4.1

--- www/templates/email/ENews/Controller.tpl.php
+++ www/templates/email/ENews/Controller.tpl.php
@@ -1,79 +1,53 @@
-<?php if (!$context->options['preview']) : ?><!DOCTYPE html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title><?php echo $context->subject; ?></title>
+<?php if (!$context->options['preview']) : ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title><?php echo $context->subject ?></title>
+
     <style type="text/css">
-        #outlook a{padding:0;}
-        .ReadMsgBody {width: 100%;}
         .ExternalClass {width: 100%;}
         .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;}
-        body, table, td, a{-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;}
-        table, td{mso-table-lspace:0pt; mso-table-rspace:0pt;}
-        img{-ms-interpolation-mode:bicubic;}
-        body{margin:0; padding:0;}
-        img{border:0; height:auto; line-height:100%; outline:none; text-decoration:none;}
-        table{border-collapse:collapse !important;}
-        body{height:100% !important; margin:0; padding:0; width:100% !important;}
-        .appleBody a {color:#999999; text-decoration: none;}
-        .appleFooter a {color:#ffffff;}
-        .appleEvents a {color: #abadb3; font-weight:bold; text-decoration: none;}
-        .appleEventsBody a {color: #81261d;; text-decoration: none;}
-        blockquote .original-only, .WordSection1 .original-only {display: none !important;}
-        blockquote table.forwarded-only, .WordSection1 table.forwarded-only {display: block !important;}
-        p {margin-top:0!important;padding-top:0!important;}
-        td.responsive-column {display: table-cell;}
-
-        table[class="wrapper-table"] {
-            min-width:100% !important;
-        }
+        body {-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;}
+        body {margin:0; padding:0;}
+        table td {border-collapse:collapse}
 
         @media screen and (max-width: 650px) {
-            table[class="wrapper"]{
+            body[yahoo] .wrapper{
                 width:100% !important;}
-            td[class="mobile-hide"]{
+            body[yahoo] .mobile-hide{
                 display:none;}
-            td[class="mobile-show"]{
+            body[yahoo] .mobile-show{
                 display:block!important;}
-            td[class="mobile-logo"] img{
-                height:54px!important;
-                width:54px!important;}
-            td[class="mobile-unl"]{
-                height:23px;}
-            td[class="mobile-unl"] img{
-                height:23px!important;
-                width:266px!important;}
-            td[class="mobile-header"]{
-                vertical-align: top;
-                font-size: 23px!important;
-                line-height: 1.1!important;}
-            td[class="head1"]{
-                font-size: 30px!important;
-                padding: 0 10px 20px!important;}
-            td[class="head2"]{
-                font-size: 16px!important;
-                padding: 25px 20px 20px 20px!important;}
-            td[class="head3"]{
-                font-size: 15px!important;
-                padding: 0 0 25px 0!important;}
-            td[class="head4"]{
-                padding: 0 20px 25px 20px !important;}
-            table[class="button"]{
+            body[yahoo] .wdn-header-top img{
+                height:30px!important;
+                width:auto!important;
+            }
+            body[yahoo] .wdn-logo{
+                width:66px!important;
+            }
+            body[yahoo] .wdn-logo img{
+                height:71px!important;
+                width:auto!important;
+            }
+            body[yahoo] .wdn-logo-lockup img{
+                height:71px!important;
+                width:auto!important;
+            }
+            body[yahoo] table.button{
                 width:85%!important;
                 height:auto}
-            td[class="button"]{
+            body[yahoo] td.button {
                 font-size: 20px!important;
                 padding: 10px!important;}
-            img[class="img-full"]{
+            body[yahoo] .img-full{
                 width:100% !important;
                 max-width: 100%;
                 height:auto;}
-            table[class="responsive-table"]{
+            body[yahoo] .responsive-table{
                 float: none!important;
                 width:100%!important;}
-            td[class="padding"]{
+            body[yahoo] .padding {
                 padding: 10px 8% 10px 8% !important;
                 text-align: center !important;}
-            td[class="section-padding"]{
+            body[yahoo] .section-padding{
                 padding: 50px 15px 50px 15px !important;}
 
             td[class="unltoday-padding"]{
@@ -99,9 +73,14 @@
         }
 
         #newsColumnIntro {padding-bottom:10px;margin-bottom:25px;}
+        p {margin-top:0!important;padding-top:0!important;}
+        td.responsive-column {display: table-cell;}
+        table[class="wrapper-table"] {
+            min-width:100% !important;
+        }
     </style>
 </head>
-<body>
+<body yahoo="fix" style="margin: 0; padding: 0; color:#5b5b5a; font-size:16px; line-height:1.777; font-family:Georgia, serif;" bgcolor="#fefdfa" link="#d00000">
 <?php endif; ?>
 
 <?php echo $savvy->render($context); ?>
--- www/templates/email/ENews/Newsletter.tpl.php
+++ www/templates/email/ENews/Newsletter.tpl.php
@@ -1,93 +1,45 @@
-<?php
-	if (!empty($context->options['preview'])) : //For the preview building tool
-?>
-<div id="previewWrapper" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
-	<a href="<?php echo $context->getURL(); ?>">Problem viewing? Click here to read online.</a>
-	<div id="preview">
-		<header>
-            <img src="http://www.unl.edu/wdn/templates_4.0/images/email/header_01.gif">
-            <img src="http://www.unl.edu/wdn/templates_4.0/images/email/header_02.gif">
-            <h1><?php echo UNL_ENews_Newsroom::getByID($context->newsroom_id)->name; ?></h1>
-		</header>
-		<div id="previewContent">
-            <div id="mast">
-                <time class="newsletterDate" datetime="<?php echo $context->release_date; ?>"><?php echo date('F j, Y', strtotime($context->release_date)); ?></time>
-                <?php if(UNL_ENews_Newsroom::getByID($context->newsroom_id)->subtitle) :?>
-                    <h2><?php echo UNL_ENews_Newsroom::getByID($context->newsroom_id)->subtitle; ?></h2>
-                <?php endif;?>
-                <div style="clear:both"></div>
-            </div>
-            
-				<table>
-				<?php
-					$stories = $context->getStories();
-					$stories->setIsPreview(true);
-					echo $savvy->render($stories, 'templates/html/ENews/Newsletter/Stories.tpl.php');
-				?>
-				</table>
-		</div>
-		<footer>
-		<p>
-			<img src="http://www.unl.edu/wdn/templates_4.0/images/email/wordmark.gif" alt="" width="111" height="43" />
-		    &copy; <?php echo date('Y'); ?> University of Nebraska&ndash;Lincoln <br />
-		    <?php echo $context->newsroom->getRaw('footer_text'); ?>
-		</p>
-		<p>
-			<a href="<?php echo $context->newsroom->getSubmitURL(); ?>">Submit Your News</a>
-		</p>
-		</footer>
-	</div>
-</div>
-<?php 
-	else: //everything else
-?>
-<table border="0" cellpadding="0" cellspacing="0" width="100%" style="line-height: normal; min-width: 650px;" class="wrapper-table">
+<?php if (!empty($context->options['preview'])) : //For the preview building tool ?>
+  <div id="previewWrapper" style="color:#494949; font-size: 12px; line-height: 140%; font-family: 'Lucida Grande',Verdana,Arial;">
+<?php endif; ?>
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
     <tr>
         <td>
             <table border="0" cellpadding="0" cellspacing="0" width="100%">
                 <tr>
-                    <td bgcolor="#F2EEE4" class="mobile-hide">
+                    <td bgcolor="#FEFDFA" class="wdn-header-top">
                         <table align="center" border="0" cellpadding="0" cellspacing="0" width="650" class="responsive-table">
                             <tr>
-                                <td style="line-height:0"><a href="http://www.unl.edu/" target="_blank"><img src="http://www.unl.edu/wdn/templates_4.0/images/email/header_01.gif" alt="" height="41" width="650" border="0" align="left" style="display: block;"></a></td>
+                                <td><a href="http://www.unl.edu/" target="_blank" style="text-decoration: none; color: #d00000;"><img src="http://www.unl.edu/images/email/4-1/41_01.gif" width="320" height="36" alt="" border="0" align="left" style="display: block;"></a></td>
                             </tr>
                         </table>
                     </td>
                 </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td style="border-top:1px solid #EAE9E6">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
                 <tr>
-                    <td bgcolor="#F8F5EC" class="mobile-hide">
+                    <td bgcolor="#FEFDFA">
                         <table align="center" border="0" cellpadding="0" cellspacing="0" width="650" class="responsive-table">
                             <tr>
-                                <td><a href="http://www.unl.edu/" target="_blank"><img src="http://www.unl.edu/wdn/templates_4.0/images/email/header_02.gif" alt="" height="30" width="650" border="0" align="left" style="display: block;"></a></td>
-                            </tr>
-                            <tr>
-                                <td align="center" style="font-size: 30px; line-height: 1.5; font-family: Georgia, serif;"><a href="<?php echo $context->getURL(); ?>" target="_blank" style="text-decoration: none; color:#444341"><?php echo UNL_ENews_Newsroom::getByID($context->newsroom_id)->name; ?></a></td>
+                                <td width="77" class="wdn-logo"><a href="http://www.unl.edu/" target="_blank"><img src="http://www.unl.edu/images/email/4-1/41_02.gif" width="77" height="83" alt="" border="0" align="left" style="display: block;"></a></td>
+                                <td valign="middle" class="wdn-logo-lockup" style="font-size:38px; line-height:1; font-family: 'Arial Narrow', Arial, sans-serif; text-transform:uppercase; white-space:nowrap"><a href="<?php echo $context->getURL(); ?>" target="_blank" style="text-decoration: none; color:#474746"><?php echo UNL_ENews_Newsroom::getByID($context->newsroom_id)->name; ?></a></td>
                             </tr>
                         </table>
                     </td>
                 </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#C80E13" style="padding: 10px 20px 10px 20px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="610" class="responsive-table" style="font-size:14px;font-family:Helvetica,Arial,sans-serif;color:#FFFFFF;">
                 <tr>
-                    <td bgcolor="#F8F5EC" style="display: none;" class="mobile-show">
-                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="650" class="responsive-table">
-                            <tr>
-                                <td class="mobile-logo" rowspan="2" width="54"><a href="http://www.unl.edu/" target="_blank"><img src="http://www.unl.edu/wdn/templates_4.0/images/email/header_05.gif" width="1" height="1" alt="" border="0" align="left" style="display: block; width: 0px; height: 0px;"></a></td>
-                                <td class="mobile-unl"><img src="http://www.unl.edu/wdn/templates_4.0/images/email/header_06.gif" width="1" height="1" alt="" border="0" align="left" style="display: block; width: 0px; height: 0px;"></td>
-                            </tr>
-                            <tr>
-                                <td class="mobile-header" style="font-size: 0px; line-height: 0; font-family: Georgia, serif;"><a href="<?php echo $context->getURL(); ?>" target="_blank" style="text-decoration: none; color: #444341;"><?php echo UNL_ENews_Newsroom::getByID($context->newsroom_id)->name; ?></a></td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-                <tr>
-                    <td bgcolor="#C80E13" style="padding: 10px 20px 10px 20px;">
-                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="610" class="responsive-table" style="font-size:14px;font-family:Helvetica,Arial,sans-serif;color:#FFFFFF;">
-                            <tr>
-                                <td class="unltoday-mast" align="left"><?php echo date('F j, Y', strtotime($context->release_date)); ?></td>
-                                <td class="unltoday-mast" align="right"><i><?php echo UNL_ENews_Newsroom::getByID($context->newsroom_id)->subtitle; ?></i></td>
-                            </tr>
-                        </table>
-                    </td>
+                    <td class="unltoday-mast" align="left"><?php echo date('F j, Y', strtotime($context->release_date)); ?></td>
+                    <td class="unltoday-mast" align="right"><i><?php echo UNL_ENews_Newsroom::getByID($context->newsroom_id)->subtitle; ?></i></td>
                 </tr>
             </table>
         </td>
@@ -114,20 +66,20 @@
 </table>
 <!-- End main content -->
 
-<table border="0" cellpadding="0" cellspacing="0" width="100%" style="line-height: normal; min-width: 650px;" class="wrapper-table">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="line-height: normal;">
     <tr>
-        <td bgcolor="#525151" align="center">
+        <td bgcolor="#333332" align="center">
             <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
                 <tr>
                     <td style="padding: 20px 20px 20px 20px;">
                         <table width="610" border="0" cellspacing="0" cellpadding="0" align="center" class="responsive-table">
                             <tr>
-                                <td align="left" valign="middle" style="font-size: 10px; line-height: 16px; font-family: Helvetica, Arial, sans-serif; color:#cfcfcf; -webkit-text-size-adjust: none">
-                                <span class="appleFooter" style="color:#cfcfcf; -webkit-text-size-adjust: none"><a style="color:#cfcfcf" href="<?php echo $context->newsroom->getSubmitURL(); ?>">Submit Your News</a> | <a style="color:#cfcfcf" href="<?php echo $context->getURL(); ?>">Read this newsletter on the web</a><br />
-                                    &copy; <?php print date("Y")?> University of Nebraska&ndash;Lincoln<br />
-                                    <?php echo $context->newsroom->getRaw('footer_text'); ?>
-                                 </span></td>
-                                <td width="111" valign="top" class="mobile-hide" style="padding-left:20px"><a href="http://www.unl.edu/" target="_blank" style="font-family: Arial; color: #ffffff; font-size: 14px;"><img alt="University of Nebraska-Lincoln" src="http://www.unl.edu/wdn/templates_4.0/images/email/wordmark.gif" width="111" height="43" style="display: block;" border="0"></a></td>
+                                <td align="left" valign="middle" style="font-size: 10px; line-height: 16px; font-family: Verdana, sans-serif; color:#adacaa; -webkit-text-size-adjust: none">
+                                    <span class="appleFooter" style="color:#adacaa; -webkit-text-size-adjust: none"><a style="color:#cfcfcf" href="<?php echo $context->newsroom->getSubmitURL(); ?>">Submit Your News</a> | <a style="color:#cfcfcf" href="<?php echo $context->getURL(); ?>">Read this newsletter on the web</a><br />
+                                        &copy; <?php print date("Y")?> University of Nebraska&ndash;Lincoln<br />
+                                        <?php echo $context->newsroom->getRaw('footer_text'); ?>
+                                     </span></td>
+                                <td width="111" valign="top" class="mobile-hide" style="padding-left:20px"><a href="http://www.unl.edu/" target="_blank" style="font-family: Verdana; color: #ffffff; font-size: 14px;"><img alt="University of Nebraska-Lincoln" src="http://www.unl.edu/images/email/4-1/wordmark.gif" width="111" height="43" style="display: block;" border="0"></a></td>
                             </tr>
                         </table>
                     </td>
@@ -136,6 +88,7 @@
         </td>
     </tr>
 </table>
-<?php 
-	endif; //end conditional for preview building tool
-?>
+
+<?php if (!empty($context->options['preview'])) : //For the preview building tool ?>
+  </div>
+<?php endif; ?>
