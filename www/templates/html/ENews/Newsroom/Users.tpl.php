9038f20cebdca0fb1e5c2467a81dca61f8529054
Eric Rasmussen <ericrasmussen1@gmail.com>
2017-07-20 14:54:44 -0500

    Switch profile pic img src from planet red to directory
    Solves the issue of PR converting - to _ in student user names

--- www/templates/html/ENews/Newsroom/Users.tpl.php
+++ www/templates/html/ENews/Newsroom/Users.tpl.php
@@ -2,7 +2,7 @@
 <ul id="userList">
 <?php
 foreach ($context as $user) {
-    echo '<li><img class="profile_pic medium" src="http://planetred.unl.edu/pg/icon/unl_'.$user->uid.'/medium/" alt="Planet Red photo for '.$user->uid.'" />
+    echo '<li><img class="profile_pic medium" src="https://directory.unl.edu/avatar/'.$user->uid.'?s=medium" alt="Profile photo of '.$user->uid.'" />
     <form action="'.UNL_ENews_Controller::getURL().'?view=newsroom" method="post">
         <input type="hidden" name="newsroom_id" value="'.$parent->context->id.'" />
         <input type="hidden" name="_type" value="removeuser" />
