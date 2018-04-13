0c4c23ccd7fd0fe2290f0e4c700ad1fb15d52fac
Eric Rasmussen <ericrasmussen1@gmail.com>
2018-04-13 10:43:52 -0500

    Fix listserv signup

--- src/UNL/ENews/Newsletter/OptOut.php
+++ src/UNL/ENews/Newsletter/OptOut.php
@@ -9,7 +9,7 @@ class UNL_ENews_Newsletter_OptOut
             throw new Exception('You must pass a newsroom email object!', 400);
         }
         $list_name = substr($options['email']->email, 0, strpos($options['email']->email, '@'));
-        $this->url = 'http://listserv.unl.edu/signup-anon/?UNSUB=1&LISTNAME='.urlencode($list_name).'&LOCKTYPE=LIST&SUCCESS_URL=';
+        $this->url = 'https://listserv.unl.edu/signup-anon/?UNSUB=1&LISTNAME='.urlencode($list_name).'&LOCKTYPE=LIST&SUCCESS_URL=';
 
         $this->url .= urlencode(UNL_ENews_Controller::getURL().'?view=thanks&_type=unsubscribe');
     }
--- www/templates/html/ENews/Newsroom/SubscribeForm.tpl.php
+++ www/templates/html/ENews/Newsroom/SubscribeForm.tpl.php
@@ -1,11 +1,12 @@
 <?php foreach (new LimitIterator($context->getOptOutEmails(), 0, 1) as $email) : ?>
     <div class="subscribe">
         <h3><?php echo $context->name; ?><span>Subscribe Today!</span></h3>
-        <form method="get" action="http://listserv.unl.edu/signup-anon/" id="subscribe">
+        <form method="post" action="https://listserv.unl.edu/signup-anon/" id="subscribe">
             <label for="address">Email</label>
             <input type="email" id="address" name="ADDRESS" value="" />
             <input type="hidden" value="<?php echo $context->getURL().'?subscribed';?>" name="SUCCESS_URL" />
             <input type="hidden" value="BOTH" name="LOCKTYPE" />
+            <input type="hidden" name="ACTION" value="SUBMIT">
             <input type="hidden" name="LISTNAME" value="<?php echo substr($email->email, 0, strpos($email->email, '@')); ?>" />
             <input type="submit" value="Subscribe" name="submit" />
         </form>
