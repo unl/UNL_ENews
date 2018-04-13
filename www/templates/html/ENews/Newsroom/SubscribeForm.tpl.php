064f7aece955c7bd0e76ff56327035dd4c657d1e
Eric Rasmussen <ericrasmussen1@gmail.com>
2018-04-13 10:52:45 -0500

    Update the success URL so that the message displays

--- www/templates/html/ENews/Newsroom/SubscribeForm.tpl.php
+++ www/templates/html/ENews/Newsroom/SubscribeForm.tpl.php
@@ -4,7 +4,7 @@
         <form method="post" action="https://listserv.unl.edu/signup-anon/" id="subscribe">
             <label for="address">Email</label>
             <input type="email" id="address" name="ADDRESS" value="" />
-            <input type="hidden" value="<?php echo $context->getURL().'?subscribed';?>" name="SUCCESS_URL" />
+            <input type="hidden" value="<?php echo $context->getURL().'?_type=subscribed';?>" name="SUCCESS_URL" />
             <input type="hidden" value="BOTH" name="LOCKTYPE" />
             <input type="hidden" name="ACTION" value="SUBMIT">
             <input type="hidden" name="LISTNAME" value="<?php echo substr($email->email, 0, strpos($email->email, '@')); ?>" />
