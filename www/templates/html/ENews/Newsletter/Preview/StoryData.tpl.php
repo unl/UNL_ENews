a59642124fa04966e816c1fe352d8dff65edf399
Jeffrey Sturek <jsturek8@unl.edu>
2019-03-04 12:18:28 -0600

    Redo javascript for story data since not always rendered on page load

--- www/templates/html/ENews/Newsletter/Preview/StoryData.tpl.php
+++ www/templates/html/ENews/Newsletter/Preview/StoryData.tpl.php
@@ -1,19 +1,18 @@
-<?php
-if ($context->getRawObject() instanceof UNL_ENews_Newsletter_Story) {
-  $savvy->loadScriptDeclaration(" 
-WDN.jQuery(\"#story_" . $context->story_id . "\").data(" . json_encode(array(
-    'id' => $context->story_id,
-    'title' => $context->story->title,
-    'presentation_id' => $context->getPresentation()->id,
-    'default_presentation_id' => $context->story->presentation->id,
-    'type' => $context->getPresentation()->type,
-    'request_publish_start' => date('M j', strtotime($context->story->request_publish_start)),
-    'request_publish_end' => isset($context->story->request_publish_end) ? date('M j', strtotime($context->story->request_publish_end)) : ''
-)) . ");");
-
-} elseif ($context->getRawObject() instanceof UNL_ENews_Story) {
-  $savvy->loadScriptDeclaration("
-WDN.jQuery(\"#story_" . $context->id . "\").data(" . json_encode(array(
+<?php if ($context->getRawObject() instanceof UNL_ENews_Newsletter_Story): ?>
+<script type="text/javascript">
+  document.addEventListener('DOMContentLoaded', function () { WDN.jQuery("#story_<?php echo $context->story_id; ?>").data(<?php echo json_encode(array(
+      'id' => $context->story_id,
+      'title' => $context->story->title,
+      'presentation_id' => $context->getPresentation()->id,
+      'default_presentation_id' => $context->story->presentation->id,
+      'type' => $context->getPresentation()->type,
+      'request_publish_start' => date('M j', strtotime($context->story->request_publish_start)),
+      'request_publish_end' => isset($context->story->request_publish_end) ? date('M j', strtotime($context->story->request_publish_end)) : ''
+      )); ?>);}, false);
+</script>
+<?php elseif ($context->getRawObject() instanceof UNL_ENews_Story): ?>
+<script type="text/javascript">
+  document.addEventListener('DOMContentLoaded', function () { WDN.jQuery("#story_<?php echo $context->id; ?>").data(<?php echo json_encode(array(
     'id' => $context->id,
     'title' => $context->title,
     'presentation_id' => $context->presentation->id,
@@ -21,6 +20,6 @@ WDN.jQuery(\"#story_" . $context->id . "\").data(" . json_encode(array(
     'type' => $context->presentation->type,
     'request_publish_start' => date('M j', strtotime($context->request_publish_start)),
     'request_publish_end' => isset($context->request_publish_end) ? date('M j', strtotime($context->request_publish_end)) : ''
-)) . ");");
-}
-?>
+  )); ?>);}, false);
+</script>
+<?php endif; ?>
