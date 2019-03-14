cca7e9b53833a9dcd7a8001de5775f00119e5468
Jeff Sturek <jsturek@gmail.com>
2019-03-14 15:23:15 -0500

    Move just image to table for padding but to allow left/right alignment with text wrapping (#82)
    * Move just image to table for padding but to allow left/right alignment with text wrapping
    
    * Tweak newsletter email image layout to display correctly across email clients

--- www/templates/email/ENews/Newsletter/Story/Presentation/News/ThumbnailFloatedRight.tpl.php
+++ www/templates/email/ENews/Newsletter/Story/Presentation/News/ThumbnailFloatedRight.tpl.php
@@ -1,61 +1,68 @@
 <table border="0" cellpadding="0" cellspacing="0" width="100%" style="line-height:normal">
-    <tr>
-        <td bgcolor="#F8F5EC" style="padding:10px 10px 10px 10px" class="unltoday-padding">
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" class="responsive-table" style="max-width: 620px;">
-                <tbody>
-                <tr>
-                    <td valign="middle">
-                        <table border="0" cellspacing="0" cellpadding="0" width="100%">
-                            <tbody>
-                            <tr>
-                                <td align="left" valign="top">
-                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                        <tbody>
+  <tr>
+    <td bgcolor="#F8F5EC" style="padding:10px 10px 10px 10px" class="unltoday-padding">
+      <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" class="responsive-table" style="max-width: 620px;">
+        <tbody>
+        <tr>
+          <td valign="middle">
+            <table border="0" cellspacing="0" cellpadding="0" width="100%">
+              <tbody>
+              <tr>
+                <td align="left" valign="top">
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                    <tr>
+                      <td colspan="2" class="unltoday-head" align="left" style="font-size:24px;font-family:Helvetica,Arial,sans-serif;color:#cc0000;padding-bottom:18px">
+                        <a href="<?php echo $context->getURL(); ?>" style="color:#cc0000;text-decoration:none;"><?php echo $context->title; ?></a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="unltoday-body" align="top" style="font-size:16px;line-height:25px;font-family:Georgia,serif;color:#545350;border-bottom:2px solid #E7E2D6;padding:0 0 6px 0;">
+                          <?php if ($file = $context->getThumbnail()) : ?>
 
-                                        <tr>
-                                            <td colspan="2" class="unltoday-head" align="left" style="font-size:24px;font-family:Helvetica,Arial,sans-serif;color:#cc0000;padding-bottom:18px">
-                                                <a href="<?php echo $context->getURL(); ?>" style="color:#cc0000;text-decoration:none;"><?php echo $context->title; ?></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="unltoday-body" align="top" style="font-size:16px;line-height:25px;font-family:Georgia,serif;color:#545350;border-bottom:2px solid #E7E2D6;padding:0 0 6px 0;">
-                                                <?php if ($file = $context->getThumbnail()) : ?>
-                                                    <img src="<?php echo $file->getURL(); ?>" width="30%" style="max-width:35%; margin:0 0 5px 8px; padding-top: 5px;" align="right" />
-                                                <?php endif; ?>
-                                                <p>
-                                                    <?php
-                                                    echo $savvy->render($context, 'ENews/Story/field-description.tpl.php');
-                                                    if (!empty($context->full_article)) {
-                                                        echo ' <a href="'.$context->getURL().'" style="color:#BA0000;">Continue reading&hellip;</a>';
-                                                    }
-                                                    ?>
-                                                    <?php if (isset($context->ics)): ?>
-                                                        <a href="<?php echo $context->ics ?>" class="icsformat">Add to my calendar (.ics)</a>
-                                                    <?php endif; ?>
-                                                </p>
-
-                                                <?php if (($context->website)): ?>
-                                                    <table cellspacing="0" cellpadding="3" border="0" valign="top" bgcolor="#E7E2D6" width="100%">
-                                                        <tr>
-                                                            <td align="right" style="padding:0 8px 0 0;font-size:14px;line-height:20px;">
-                                                                More details at <a href="<?php echo $context->website; ?>" title="Go to the supporting webpage" style="color:#BA0000;"><?php echo $context->website; ?></a>
-                                                            </td>
-                                                        </tr>
-                                                    </table>
-                                                <?php endif; ?>
-                                            </td>
-                                        </tr>
-
-                                        </tbody>
-                                    </table>
+                            <table width="40%" border="0" cellspacing="0" cellpadding="0" align="right" style="width:40%; max-width:40%; margin-left: 10px">
+                              <tbody>
+                              <tr>
+                                <td valign="top">
+                                  <img width="100%" src="<?php echo $file->getURL(); ?>" style="width: 100%" />
                                 </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </td>
-                </tr>
-                </tbody>
+                              </tr>
+                              </tbody>
+                            </table>
+                          <?php endif; ?>
+                        <p>
+                            <?php
+                            echo $savvy->render($context, 'ENews/Story/field-description.tpl.php');
+                            if (!empty($context->full_article)) {
+                                echo ' <a href="'.$context->getURL().'" style="color:#BA0000;">Continue reading&hellip;</a>';
+                            }
+                            ?>
+                            <?php if (isset($context->ics)): ?>
+                              <a href="<?php echo $context->ics ?>" class="icsformat">Add to my calendar (.ics)</a>
+                            <?php endif; ?>
+                        </p>
+
+                          <?php if (($context->website)): ?>
+                            <table cellspacing="0" cellpadding="3" border="0" valign="top" bgcolor="#E7E2D6" width="100%">
+                              <tr>
+                                <td align="right" style="padding:0 8px 0 0;font-size:14px;line-height:20px;">
+                                  More details at <a href="<?php echo $context->website; ?>" title="Go to the supporting webpage" style="color:#BA0000;"><?php echo $context->website; ?></a>
+                                </td>
+                              </tr>
+                            </table>
+                          <?php endif; ?>
+                      </td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              </tbody>
             </table>
-        </td>
-    </tr>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </td>
+  </tr>
 </table>
