d703551b89aa90e5a774ba51c24610922e68a7f3
Eric Rasmussen <ericrasmussen1@gmail.com>
2019-05-23 11:20:47 -0500

    Improve the forms on the newsroom details edit page (view=newsroom)

--- www/templates/html/ENews/Newsroom/AddUserForm.tpl.php
+++ www/templates/html/ENews/Newsroom/AddUserForm.tpl.php
@@ -1,9 +1,12 @@
-<form action="<?php echo UNL_ENews_Controller::getURL(); ?>?view=newsroom" method="post" id="addUser" class="addData">
+<form action="<?php echo UNL_ENews_Controller::getURL(); ?>?view=newsroom" method="post" id="addUser" class="dcf-form-controls-inline addData">
     <?php $csrf = UNL_ENews_Controller::getCSRFHelper() ?>
     <input type="hidden" name="<?php echo $csrf->getTokenNameKey() ?>" value="<?php echo $csrf->getTokenName() ?>" />
     <input type="hidden" name="<?php echo $csrf->getTokenValueKey() ?>" value="<?php echo $csrf->getTokenValue() ?>">
     <input type="hidden" name="newsroom_id" value="<?php echo $context->id; ?>" />
     <input type="hidden" name="_type" value="adduser" />
-    <input type="text" name="user_uid" />
-    <input type="submit" value="Add User" />
+    <label for="dcf-input-group-button-input">My.UNL username</label>
+    <div class="dcf-input-group">
+        <input type="text" class="dcf-input-group-button-input" name="user_uid" />
+        <input type="submit" class="dcf-btn dcf-btn-primary" value="Add User" />
+    </div>
 </form>
--- www/templates/html/ENews/Newsroom/Users.tpl.php
+++ www/templates/html/ENews/Newsroom/Users.tpl.php
@@ -1,9 +1,9 @@
-<h3 class="dcf-pt-4">Users</h3>
-<ul id="userList" class="dcf-list-inline">
+<h3 class="dcf-mt-6">Users</h3>
+<ul id="userList" class="dcf-list-inline unl-bg-lighter-gray dcf-p-2">
 <?php foreach ($context as $user): ?>
-    <li>
-      <img class="profile_pic medium" src="https://directory.unl.edu/avatar/<?php echo $user->uid ?>?s=medium" alt="Profile photo of <?php echo $user->uid ?>" />
-      <form action="<?php echo UNL_ENews_Controller::getURL() ?>?view=newsroom" method="post">
+    <li class="dcf-p-1 dcf-relative">
+      <img class="dcf-d-block" src="https://directory.unl.edu/avatar/<?php echo $user->uid ?>?s=medium" alt="Profile photo of <?php echo $user->uid ?>" />
+      <form class="dcf-absolute dcf-d-none" action="<?php echo UNL_ENews_Controller::getURL() ?>?view=newsroom" method="post">
           <?php $csrf = UNL_ENews_Controller::getCSRFHelper() ?>
           <input type="hidden" name="<?php echo $csrf->getTokenNameKey() ?>" value="<?php echo $csrf->getTokenName() ?>" />
           <input type="hidden" name="<?php echo $csrf->getTokenValueKey() ?>" value="<?php echo $csrf->getTokenValue() ?>">
@@ -12,7 +12,7 @@
           <input type="hidden" name="user_uid" value="<?php echo $user->uid ?>" />
           <input class="dcf-btn" type="submit" value="Remove" />
       </form>
-      <span class="uid"><?php echo $user->uid ?></span>
+      <span class="dcf-txt-xs"><?php echo $user->uid ?></span>
     </li>
 <?php endforeach; ?>
 </ul>
