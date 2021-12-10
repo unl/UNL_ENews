848e7044125bb09452fffa27a2c65360a0f2575d
Jeff Sturek <jsturek@gmail.com>
2021-12-10 09:15:50 -0600

    Removefirst/last pagination buttons

--- www/templates/html/ENews/PaginationLinks.tpl.php
+++ www/templates/html/ENews/PaginationLinks.tpl.php
@@ -3,14 +3,10 @@
     $cpFudge = $context->limit == 1 ? 2 : 1;
     $currentPage = intval(ceil(($context->offset  - 1) / $context->limit) + $cpFudge);
     $numberOfPages = intval(ceil($context->total / $context->limit));
-    $showFirstLast = FALSE; //$numberOfPages > 10;
 ?>
 <nav class="dcf-pagination dcf-txt-center">
     <ol class="dcf-list-bare dcf-list-inline">
     <?php if ($context->offset != 0) :?>
-        <?php if ($showFirstLast): ?>
-            <li><a class="dcf-pagination-first" href="<?php echo UNL_ENews_Controller::addURLParams($context->url, array('limit'=>$context->limit, 'offset'=>0)); ?>">First</a></li>
-        <?php endif; ?>
         <li><a class="dcf-pagination-prev" href="<?php echo UNL_ENews_Controller::addURLParams($context->url, array('limit'=>$context->limit, 'offset'=>($context->offset-$context->limit))); ?>">Prev</a></li>
     <?php endif; ?>
     <?php
@@ -34,9 +30,6 @@
     <?php } // end for ?>
     <?php if (($context->offset+$context->limit) < $context->total) :?>
         <li><a class="dcf-pagination-next" href="<?php echo UNL_ENews_Controller::addURLParams($context->url, array('limit'=>$context->limit, 'offset'=>($context->offset+$context->limit))); ?>">Next</a></li>
-	    <?php if ($showFirstLast): ?>
-            <li><a class="dcf-pagination-last" href="<?php echo UNL_ENews_Controller::addURLParams($context->url, array('limit'=>$context->limit, 'offset'=>($numberOfPages-1) * $context->limit)); ?>">Last</a></li>
-	    <?php endif; ?>
     <?php endif; ?>
     </ol>
 </nav>
