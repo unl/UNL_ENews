2bc7ddc358f0ea89f43cbf011ce8d6e4bfe3293a
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2024-01-23 12:09:09 -0600

    Added size to the avatar image (#106)
    * Added size to the avatar image
    
    * Remove stank

--- www/templates/html/ENews/Newsroom/Users.tpl.php
+++ www/templates/html/ENews/Newsroom/Users.tpl.php
@@ -2,7 +2,11 @@
 <ul id="userList" class="dcf-list-inline unl-bg-lighter-gray dcf-p-2">
 <?php foreach ($context as $user): ?>
     <li class="dcf-p-1 dcf-relative">
-      <img class="dcf-d-block" src="https://directory.unl.edu/avatar/<?php echo $user->uid ?>?s=medium" alt="Profile photo of <?php echo $user->uid ?>" />
+      <img
+        class="dcf-d-block dcf-h-10 dcf-w-10"
+        src="https://directory.unl.edu/avatar/<?php echo $user->uid ?>?s=medium"
+        alt="Profile photo of <?php echo $user->uid ?>"
+      />
       <form class="dcf-form dcf-absolute dcf-d-none" action="<?php echo UNL_ENews_Controller::getURL() ?>?view=newsroom" method="post">
           <?php $csrf = UNL_ENews_Controller::getCSRFHelper() ?>
           <input type="hidden" name="<?php echo $csrf->getTokenNameKey() ?>" value="<?php echo $csrf->getTokenName() ?>" />
