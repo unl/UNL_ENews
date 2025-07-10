b18ca39202865c55f7a97f11f046f089aeaf0a52
Thomas Neumann <57959550+tommyneu@users.noreply.github.com>
2025-07-10 13:27:27 -0500

    Removed error from empty newsroom home page (#119)

--- www/templates/html/ENews/Newsletter/Public.tpl.php
+++ www/templates/html/ENews/Newsletter/Public.tpl.php
@@ -1,1 +1,8 @@
 <?php echo $savvy->render($context->newsletter); ?>
+<?php if (!$context->newsletter): ?>
+    <?php
+        UNL_ENews_PostRunFilter::setReplacementData('pagetitle', $context->newsroom->name);
+        UNL_ENews_PostRunFilter::setReplacementData('sitetitle', $context->newsroom->name);
+    ?>
+    <p>This newsroom currently has no published newsletters.</p>
+<?php endif; ?>
